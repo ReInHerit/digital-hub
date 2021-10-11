07b89465e33bcebfed158c13fb6ca91993d9a14f
Sebastian Stoff <sebastianstoff@gmx.at>
2021-10-11 14:02:14 +0200

    #5. Added own component for the inclusion of netlify cms admin portal

---
+++ src/components/shared/NetlifyCMSAdmin/index.tsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+/**
+ * Component adds NetlifyCMSAdmin portal when rendered. 
+ * @returns 
+ */
+const NetlifyCMSAdmin = () => {
+  return <script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"></script>
+}
+
+export default NetlifyCMSAdmin;
