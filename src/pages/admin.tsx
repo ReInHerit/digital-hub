5b23583a3c26861d78631898e3cf242af990dc6a
Sebastian Stoff <sebastianstoff@gmx.at>
2021-10-11 13:37:34 +0200

    #5, using script cdn to solve ssr bug

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,19 +1,25 @@
 import React from "react"
-import CMS from 'netlify-cms-app'
+//import CMS from 'netlify-cms-app'
 
 
 const Admin = () => {
 
-  // Initialize the CMS object
-  CMS.init();
-  // Now the registry is available via the CMS object.
-  // CMS.registerPreviewTemplate('my-template', MyTemplate)
+  // Check if window is defined (so if in the browser or in node.js).
+  const isBrowser = typeof window !== "undefined"
+  // run CMS.init() only in browser
+  //if(isBrowser){
+    // Initialize the CMS object
+    //CMS.init();
+    // Now the registry is available via the CMS object.
+    // CMS.registerPreviewTemplate('my-template', MyTemplate)
+  //}
+
+  
 
 
   return <> 
     {/* <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script> */}
-    {/* <script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"></script> */}
-    
+    <script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"></script>
   </>
 }
 
