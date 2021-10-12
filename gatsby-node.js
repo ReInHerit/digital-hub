f36659b74399e3336f77211752d469d45c9bd482
Sebastian Stoff <sebastianstoff@gmx.at>
2021-10-12 09:59:51 +0200

    #10. testing out build script for automatic setting local_backend to false when in build mode

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,53 +1,28 @@
-const path = require('path')
+const path = require('path');
+const fs = require('fs'); //import filesystem module
 
 module.exports.createPages = async ({ graphql, actions}) => {
     const { createPage } = actions
 
-    // querying local files on file system
-  //   const responseFileSystem = await graphql(`
-  //   query {
-  //     allFile(filter: {sourceInstanceName: {eq: "doc"}, relativePath: {regex: "/.md/"}}) {
-  //       edges {
-  //         node {
-  //           name
-  //           id
-  //           extension
-  //           sourceInstanceName
-  //           size
-  //           relativePath
-  //           modifiedTime(fromNow: true)
-  //           birthTime(formatString: "DD.MM.YYYY")
-  //           childrenMarkdownRemark {
-  //             excerpt
-  //             html
-  //             headings {
-  //               value
-  //             }
-  //             tableOfContents
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  //   `);
-
-  //   const mdTemplate = path.resolve('./src/templates/markdown.js')
-
-  //   // use data to create page for each npm node
-  //   responseFileSystem.data.allFile.edges.forEach((edge) => {
-  //     if(edge.node.extension !== "md")return;
-  //     createPage({
-  //         component: mdTemplate,
-  //         path: `/doc/${edge.node.relativePath}`,
-  //         context: {
-  //             id: edge.node.id
-  //         }
-  //     })
-  // })
-
-
-
 }
+
+
+exports.onPostBuild = () => {
+  console.log("*********************onPOSTBUILD****************");
+  console.log("CUSTOM-Script: ...setting netlify cms config local backend to false");
+  console.log("************************************************");
+
+  // set netlify-cms local backend parameter to false when building the app.
+  fs.readFile(".\\public\\admin\\config.yml",'utf8', (err, data) => {
+    if(err)return;
+    let refFile = data.replace(/local_backend: true/g, "local_backend: false");
+
+    fs.writeFile(".\\public\\admin\\config.yml", refFile, 'utf8', function(err) {
+      if (err) return console.log(err);
+      console.log("WROTE NEW FILE");
+    });
+  });
+ }
 
 
 exports.onCreatePage = async ({ page, actions }) => {
