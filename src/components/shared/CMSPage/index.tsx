8c448b51081f93fe02c6c805cc573ef61d3caf21
Sebastian Stoff <sebastianstoff@gmx.at>
2021-10-12 11:46:04 +0200

    #7. Added component to actively fetch user content from static json

---
+++ src/components/shared/CMSPage/index.tsx
@@ -0,0 +1,46 @@
+import React from "react"
+
+interface Props {
+  url: string
+}
+
+interface CMSPageModel {
+  title: string
+  intro: string
+}
+
+/**
+ * Requests a specific page json from given url and renders a view accordingly.
+ * @param props 
+ * @returns 
+ */
+const CMSPage: React.FC<Props> = props => {
+  const [pageData, setPageData] = React.useState<CMSPageModel | undefined>()
+
+  React.useEffect(() => {
+    fetchPageData(props.url).then(respData => setPageData(respData))
+  }, [])
+
+  const fetchPageData = async (url: string) => {
+    let resp
+    let respData: CMSPageModel
+
+    try {
+      resp = await fetch(props.url)
+      respData = await resp.json()
+      return respData
+    } catch (e) {
+      console.error(`Failed fetching CMSPage data from: ${url}`)
+      console.error(e)
+    }
+  }
+
+  return pageData ? (
+    <div>
+      <p>{pageData.title && pageData.title}</p>
+      <p>{pageData.intro && pageData.intro}</p>
+    </div>
+  ) : null
+}
+
+export default CMSPage
