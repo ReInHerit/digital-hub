e0cdb5dafba76d6f9549ca0f76fe9ce8112d9484
Sebastian Stoff <sebastianstoff@gmx.at>
2021-10-14 12:10:06 +0200

    #6. Added example how we could use mode parameter to display different page layouts

--- src/pages/portal/index.tsx
+++ src/pages/portal/index.tsx
@@ -1,8 +1,35 @@
 import React from "react";
+import { useLocation } from '@reach/router';
 
 const PortalPage = () => {
 
-  return <p>Portal page</p>
+  // --> has to be solved via gatsby's graphql layer and not via reading out location?
+  // see https://www.gatsbyjs.com/docs/adding-pagination/
+  // see https://www.gatsbyjs.com/docs/adding-app-and-website-functionality/
+  const loc = useLocation();
+
+  const [modeVal, setModeVal] = React.useState(0);
+
+  // gatsby wouldn't run useffect 
+  React.useEffect(() => {
+    setModeVal(handleLoc());
+  }, []);
+
+  const handleLoc = () => {
+    if (typeof window === "undefined") return 1;
+    
+    let url = new URL(loc.href);
+  
+    console.log(url.searchParams.get("mode"));
+    let modeVal = parseInt(url.searchParams.get("mode"));
+    return modeVal;
+  }
+
+ 
+
+  return <p style={{color: modeVal < 100 ? "red" : "green"}}>Portal page</p>
+
+  
 
 }
 
