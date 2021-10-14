73ccff6dfd32f1a4e61645efc71ab4ae00513900
Sebastian Stoff <sebastianstoff@gmx.at>
2021-10-14 13:12:05 +0200

    #21. removed unneeded components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,29 +2,17 @@ import React from "react"
 
 import SEO from "../components/seo"
 import Hero from "../components/hero"
-import Services from "../components/services"
-import AboutPanel from "../components/about-panel"
-import Testimonials from "../components/testimonials"
-import Articles from "../components/articles"
 import ThemeProvider from "@material-ui/styles/ThemeProvider"
 import theme from "../theme"
-import AppsPanel from "../components/apps-panel"
 import BaseLayout from "../components/static/BaseLayout"
 
 const IndexPage = () => (
   <React.Fragment>
     <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-    <ThemeProvider theme={theme}>
       <BaseLayout>
         <SEO title="Home" />
           <Hero />
-          <Services />
-          <AboutPanel />
-          <AppsPanel />
-          {/* <Testimonials /> */}
-          <Articles />
       </BaseLayout>
-    </ThemeProvider>
   </React.Fragment>
 )
 
