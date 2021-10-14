f91788af9e89c90fe487fad727daadc030db4c66
Sebastian Stoff <sebastianstoff@gmx.at>
2021-10-14 14:04:21 +0200

    #21. updated files dependent on material ui

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,19 +1,15 @@
 import React from "react"
 import SEO from "../components/seo"
-import ThemeProvider from "@material-ui/styles/ThemeProvider"
-import theme from "../theme"
 import CMSPage from "../components/shared/CMSPage"
 import BaseLayout from "../components/static/BaseLayout"
 
 const AboutPage = () => (
   <React.Fragment>
-    <ThemeProvider theme={theme}>
       <BaseLayout>
       <SEO title="About" />
         <h1>About page</h1>
         <CMSPage url="/sites/about.json"/>
       </BaseLayout>
-    </ThemeProvider>
   </React.Fragment>
 )
 
