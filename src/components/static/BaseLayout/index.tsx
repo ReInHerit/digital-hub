3b3984c6a945fa594ab3b6ab87818c4fd525c981
Sebastian Stoff <sebastianstoff@gmx.at>
2021-10-15 16:29:12 +0200

    #17. added fontawesome bug fix

--- src/components/static/BaseLayout/index.tsx
+++ src/components/static/BaseLayout/index.tsx
@@ -15,6 +15,13 @@ import { ReinheritRESTMode } from "../../../types/reinherit"
 import { useCaseMode } from "../../../hooks/useCaseMode"
 import { useReinMode } from "../../../hooks/useReinMode"
 
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; /* eslint-disable import/first */
+
 interface Props {
   children?: any
 }
