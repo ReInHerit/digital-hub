c1b3d7d9146a9440be3f948bb864aae130bedf6b
Sebi <SebastianStoff@gmx.at>
2021-10-17 19:46:15 +0200

    #18. added context based useTheme hook

---
+++ src/hooks/contexts/useTheme.tsx
@@ -0,0 +1,36 @@
+
+import React from "react";
+import { reinheritStatics } from "../../data/reinheritStatics";
+import { reinheritThemes } from "../../data/reinheritThemes";
+import { ReinheritRESTMode } from "../../types/reinherit";
+import { useReinMode } from "../useReinMode";
+
+const ReinheritThemeContext = React.createContext({mode: reinheritStatics.VISITOR.REST_MODE_VAL, styles: reinheritThemes.VISITOR});
+
+/**
+ * Hook handling reading of mode REST-Param and mapping it to the related 
+ * theme in Reinherit.
+ * @returns context specific provider hook and context itself. Additionally variables to enable usage of the hook.
+ */
+export const useTheme = () => {
+
+  // retrieves mode + active-theme
+  const rein = useReinMode();
+
+  const contextObj = {
+    mode: rein.modeVal as ReinheritRESTMode,
+    styles: rein.theme
+  }
+
+  // provider component
+  const ReinheritThemeProvider: React.FC = ({ children }) => <ReinheritThemeContext.Provider value={contextObj}>{children}</ReinheritThemeContext.Provider>
+
+  // value provided by themeprovider
+  const theme = React.useContext(ReinheritThemeContext);
+
+  return {
+    theme,
+    ReinheritThemeProvider,
+    ReinheritThemeContext
+  }
+}
