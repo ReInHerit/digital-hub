6e59e54a26daebe30286220b1139d5599bb26558
Sebastian Stoff <sebastianstoff@gmx.at>
2021-10-18 19:29:15 +0200

    #6. Added some sample content to the portal page

--- src/components/shared/PortalContent/index.tsx
+++ src/components/shared/PortalContent/index.tsx
@@ -1,8 +1,9 @@
 import { Link } from "gatsby";
 import React, { useContext } from "react";
-import { Breadcrumb } from "react-bootstrap";
+import { Breadcrumb, Card, Col, Row } from "react-bootstrap";
 import { reinheritStatics } from "../../../data/reinheritStatics";
 import { useReinTheme } from "../../../hooks/contexts/useReinTheme";
+import { REINHERIT_AUDIENCE } from "../../../types/reinherit";
 
 
 const PortalContent = () => {
@@ -17,6 +18,23 @@ const PortalContent = () => {
       </Breadcrumb>
       <h1 className={`text-${theme.styles.BS_COLOR_CLASS}`}>{reinheritStatics[theme.groupKey].LABEL}</h1>
 
+      <Row xs={1} md={2} lg={3} xl={3} className="g-5">
+      {[1,2,3,4,5,6].map((val, i) => <Col key={"col_" + i}>
+        <Link to="#" className="text-decoration-none text-dark">
+          <Card>
+            <Card.Body>
+              <Card.Title>{val}</Card.Title>
+              <Card.Text>
+                This is a longer card with 
+              </Card.Text>
+              <br />
+              <br />
+              <br />
+            </Card.Body>
+          </Card>
+        </Link>
+      </Col>)}
+    </Row>
     </>)
 
 }
