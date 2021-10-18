ba0dc824b6dec954e74b0cea21c08b2132e1e831
Sebastian Stoff <sebastianstoff@gmx.at>
2021-10-18 09:32:30 +0200

    #18. added handleTHemechange method

--- src/hooks/contexts/useTheme.tsx
+++ src/hooks/contexts/useTheme.tsx
@@ -5,7 +5,7 @@ import { reinheritThemes } from "../../data/reinheritThemes";
 import { ReinheritRESTMode } from "../../types/reinherit";
 import { useReinMode } from "../useReinMode";
 
-const ReinheritThemeContext = React.createContext({mode: reinheritStatics.VISITOR.REST_MODE_VAL, styles: reinheritThemes.VISITOR});
+const ReinheritThemeContext = React.createContext({mode: reinheritStatics.VISITOR.REST_MODE_VAL, styles: reinheritThemes.VISITOR, changeTheme: (val: string) => undefined});
 
 /**
  * Hook handling reading of mode REST-Param and mapping it to the related 
@@ -17,16 +17,27 @@ export const useTheme = () => {
   // retrieves mode + active-theme
   const rein = useReinMode();
 
+  /**
+   * Handles changing of reinherit modes dependent on REST parameter.
+   * @param themeMode 
+   */
+  const handleThemeChange = (themeMode: string) => {
+    rein.setMode(themeMode)
+  }
+
   const contextObj = {
+    changeTheme: handleThemeChange,
     mode: rein.modeVal as ReinheritRESTMode,
     styles: rein.theme
   }
+
+ 
 
   // provider component
   const ReinheritThemeProvider: React.FC = ({ children }) => <ReinheritThemeContext.Provider value={contextObj}>{children}</ReinheritThemeContext.Provider>
 
   // value provided by themeprovider
-  const theme = React.useContext(ReinheritThemeContext);
+  const theme = React.useContext(ReinheritThemeContext); 
 
   return {
     theme,
