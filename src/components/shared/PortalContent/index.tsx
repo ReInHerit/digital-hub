51df61af56ad86bb90f9faef3e93c087439b8f61
Sebastian Stoff <sebastianstoff@gmx.at>
2021-10-18 19:22:05 +0200

    #6. added breadcumb from sinas blog

--- src/components/shared/PortalContent/index.tsx
+++ src/components/shared/PortalContent/index.tsx
@@ -1,4 +1,6 @@
+import { Link } from "gatsby";
 import React, { useContext } from "react";
+import { Breadcrumb } from "react-bootstrap";
 import { reinheritStatics } from "../../../data/reinheritStatics";
 import { useReinTheme } from "../../../hooks/contexts/useReinTheme";
 
@@ -7,7 +9,15 @@ const PortalContent = () => {
 
   const { theme } = useReinTheme();
 
-  return <h1 className={`text-${theme.styles.BS_COLOR_CLASS}`}>Portal page - {reinheritStatics[theme.groupKey].LABEL}</h1>
+  return (
+    <>
+      <Breadcrumb>
+        <Breadcrumb.Item><Link to={`/?mode=${theme.mode}`}>Home</Link></Breadcrumb.Item>
+        <Breadcrumb.Item>{ reinheritStatics[theme.groupKey].LABEL} Portal</Breadcrumb.Item>
+      </Breadcrumb>
+      <h1 className={`text-${theme.styles.BS_COLOR_CLASS}`}>{reinheritStatics[theme.groupKey].LABEL}</h1>
+
+    </>)
 
 }
 
