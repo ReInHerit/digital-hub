333e7a9451601b1c5f9017c07c24805e508e08ab
Sebastian Stoff <sebastianstoff@gmx.at>
2021-10-18 15:18:00 +0200

    #6. display page now according to text color

--- src/components/shared/PortalContent/index.tsx
+++ src/components/shared/PortalContent/index.tsx
@@ -7,7 +7,7 @@ const PortalContent = () => {
 
   const { theme } = useReinTheme();
 
-  return <h1 className={`bg-${theme.styles.BS_COLOR_CLASS}`}>Portal page - {reinheritStatics[theme.groupKey].LABEL}</h1>
+  return <h1 className={`text-${theme.styles.BS_COLOR_CLASS}`}>Portal page - {reinheritStatics[theme.groupKey].LABEL}</h1>
 
 }
 
