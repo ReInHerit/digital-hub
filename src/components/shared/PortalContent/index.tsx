81490440e198e3930d4ca1cab59e7304209d2c07
Sebastian Stoff <sebastianstoff@gmx.at>
2021-10-19 13:51:51 +0200

    #6. added training page according to mode

--- src/components/shared/PortalContent/index.tsx
+++ src/components/shared/PortalContent/index.tsx
@@ -17,23 +17,40 @@ const PortalContent = () => {
         <Breadcrumb.Item>{ reinheritStatics[theme.groupKey].LABEL} Portal</Breadcrumb.Item>
       </Breadcrumb>
       <h1 style={{color: theme.styles.MAIN_COLOR}}>{reinheritStatics[theme.groupKey].LABEL}</h1>
-
+      <h2>Explore the content of the web page</h2>
       <Row xs={1} md={2} lg={3} xl={3} className="g-5">
-      {theme.groupKey === "VISITOR" ? [1,2,3,4,5,6].map((val, i) => <Col key={"col_" + i}>
-        <Link to="#" className="text-decoration-none text-dark">
-          <Card>
-            <Card.Body>
-              <Card.Title>{val}</Card.Title>
-              <Card.Text>
-                This is a longer card with 
-              </Card.Text>
-              <br />
-              <br />
-              <br />
-            </Card.Body>
-          </Card>
-        </Link>
-      </Col>) : <h2>Only for visitors render something!</h2>}
+
+        <Col key={"col_1"}>
+          <Link to={`/content/training?mode=${reinheritStatics[theme.groupKey].REST_MODE_VAL}`} className="text-decoration-none text-dark">
+            <Card className="shadow">
+              <Card.Body>
+                <Card.Title>Training</Card.Title>
+                <Card.Text>
+                  This is a longer card with 
+                </Card.Text>
+                <br />
+                <br />
+                <br />
+              </Card.Body>
+            </Card>
+          </Link>
+        </Col>
+        
+        {theme.groupKey === "VISITOR" ? [2,3,4,5,6].map((val, i) => <Col key={"col_" + i}>
+          <Link to="#" className="text-decoration-none text-dark">
+            <Card className="shadow">
+              <Card.Body>
+                <Card.Title>{val}</Card.Title>
+                <Card.Text>
+                  This is a longer card with 
+                </Card.Text>
+                <br />
+                <br />
+                <br />
+              </Card.Body>
+            </Card>
+          </Link>
+        </Col>) : <h2>Only for visitors render rest!</h2>}
     </Row>
     </>)
 
