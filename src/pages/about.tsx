96df162e612d61de9267959ac2d5c752ab6ede90
Sebi <SebastianStoff@gmx.at>
2021-11-11 13:55:59 +0100

    #39 Added two showcasesfor embedment of netlify CMS

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import SEO from "../components/seo"
 import CMSPage from "../components/shared/CMSPage"
+import NetlifyEmbed from "../components/shared/NetlifyEmbed"
 import BaseLayout from "../components/static/BaseLayout"
 
 const AboutPage = () => (
@@ -9,6 +10,7 @@ const AboutPage = () => (
       <SEO title="About" />
         <h1>About page</h1>
         <CMSPage url="/sites/about.json"/>
+        <NetlifyEmbed htmlLoc="/admin/admin.html#/collections/pages/entries/about" style={{width:"100%", height:"100vh", border:"2px solid lightgrey"}}/>
       </BaseLayout>
   </React.Fragment>
 )
--- src/pages/content/training/index.tsx
+++ src/pages/content/training/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import NetlifyEmbed from "../../../components/shared/NetlifyEmbed";
 import TrainingFeed from "../../../components/shared/TrainingFeed";
 import BaseLayout from "../../../components/static/BaseLayout";
 
@@ -8,6 +9,7 @@ const TrainingPage = () => {
   return (
     <BaseLayout>
       <h1>Training material </h1> 
+      <NetlifyEmbed htmlLoc="/admin/admin.html#/" style={{width:"100%", height:"100vh", border:"2px solid lightgrey"}}/>
       <TrainingFeed></TrainingFeed> 
     </BaseLayout>
   )
