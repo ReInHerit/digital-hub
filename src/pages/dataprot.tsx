0df9d75808e83d2111d6cbd47b88948772fe2a6e
Sebi <SebastianStoff@gmx.at>
2021-11-12 10:39:59 +0100

    #34. #41. removed dep toast ui because of build errors

--- src/pages/dataprot.tsx
+++ src/pages/dataprot.tsx
@@ -1,9 +1,6 @@
 import React from "react"
 import BaseLayout from "../components/static/BaseLayout"
 
-import 'tui-image-editor/dist/tui-image-editor.css';
-import ImageEditor from '@toast-ui/react-image-editor';
-
 const myTheme = {
   // Theme object to extends default dark theme.
 };
@@ -12,29 +9,12 @@ const DataProtectionPage = () => {
   return (
     <BaseLayout>
       <h1>Data protection</h1>
-      <ImageEditor
-    includeUI={{
-      loadImage: {
-        path: '/images/ReinHerit-1.jpg',
-        name: 'SampleImage',
-      },
-      theme: myTheme,
-      menu: ['shape', 'filter'],
-      initMenu: 'filter',
-      uiSize: {
-        width: '1000px',
-        height: '700px',
-      },
-      menuBarPosition: 'bottom',
-    }}
-    cssMaxHeight={500}
-    cssMaxWidth={700}
-    selectionStyle={{
-      cornerSize: 20,
-      rotatingPointOffset: 70,
-    }}
-    usageStatistics={false}
-  />
+      
+      <link
+        rel="stylesheet"
+        href="https://uicdn.toast.com/tui-image-editor/latest/tui-image-editor.css"
+      />
+      <script src="https://uicdn.toast.com/tui-image-editor/latest/tui-image-editor.js"></script>
     </BaseLayout>
   )
 }
