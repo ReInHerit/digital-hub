3a2d4d3de09715c41e86c5b55f9b798885e3ce1a
Sebi <SebastianStoff@gmx.at>
2021-11-13 19:26:34 +0100

    #34. added small demo code

--- src/pages/dataprot.tsx
+++ src/pages/dataprot.tsx
@@ -4,11 +4,23 @@ import BaseLayout from "../components/static/BaseLayout"
 
 const DataProtectionPage = () => {
 
+  const [url, setUrl] = React.useState("https://picsum.photos/id/237/200/300");
+
+  React.useEffect(() => {
+    // small example code increasing the counter for the id accessed every 5 secs
+    let counter = 1;
+    setInterval(() => {
+      counter++;
+      setUrl(`https://picsum.photos/id/${counter}/200/300`)
+    }, 5000)
+
+  }, [])
+
   return (
     <BaseLayout>
       <h1>Data protection</h1>
       
-      <ImageEditor></ImageEditor>
+      <ImageEditor imgAddress={url}></ImageEditor>
       
     </BaseLayout>
   )
