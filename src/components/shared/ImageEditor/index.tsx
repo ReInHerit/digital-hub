f4c9d9537f083bd13836ffb6f451613b0731f216
Sebi <SebastianStoff@gmx.at>
2021-11-13 19:12:44 +0100

    #34. common js image manipulation

--- src/components/shared/ImageEditor/index.tsx
+++ src/components/shared/ImageEditor/index.tsx
@@ -1,36 +1,61 @@
 import React from "react"
 
-import 'tui-image-editor/dist/tui-image-editor.css';
-// also necessary see:
-// https://github.com/nhn/tui.image-editor/issues/153
-import "tui-color-picker/dist/tui-color-picker.css";
 
-import TUIImageEditor from '@toast-ui/react-image-editor';
+// maybe need more deps like FileSaver for downloading stuff
 
+//according to: http://nhn.github.io/tui.image-editor/latest/#via-package-manager
 
 const ImageEditor = () => {
-  return <TUIImageEditor
-  includeUI={{
-    loadImage: {
-      path: '/images/ReInHerit-1.jpg',
-      name: 'SampleImage',
-    },
-    menu: ['shape', 'filter','text'],
-    initMenu: 'filter',
-    uiSize: {
-      width: '1000px',
-      height: '700px',
-    },
-    menuBarPosition: 'bottom',
-  }}
-  cssMaxHeight={500}
-  cssMaxWidth={700}
-  selectionStyle={{
-    cornerSize: 20,
-    rotatingPointOffset: 70,
-  }}
-  usageStatistics={false}
+
+  const isBrowser = typeof window !== "undefined"
+
+  React.useEffect(() => {
+
+    if(isBrowser){
+
+        console.log("RUN!");
+
+      // Create image editor
+      //@ts-ignore
+      const ImageEditor = require('tui-image-editor');
+
+      // options according to: http://nhn.github.io/tui.image-editor/latest/ImageEditor
+      const instance = new ImageEditor(document.querySelector('#tui-image-editor'), {
+        includeUI: {
+          loadImage: {
+            // path: '/images/ReInHerit-1.jpg',
+            path: 'https://picsum.photos/id/237/200/300',
+            name: 'SampleImage',
+          },
+          initMenu: 'filter',
+          menuBarPosition: 'bottom',
+          uiSize: {
+            width: "70vw",
+            height: "100vh"
+          },
+        },
+        cssMaxWidth: 1000,
+        cssMaxHeight: 800,
+        
+        selectionStyle: {
+          cornerSize: 20,
+          rotatingPointOffset: 70,
+        },
+      });
+
+    }
+
+  }, [])
+
+  return <>
+  <link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/tui-image-editor/3.15.2/tui-image-editor.css"
 />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tui-color-picker@2.2.7/dist/tui-color-picker.css"></link>
+
+<div style={{height:"1000px !important", width:"1000px"}} id="tui-image-editor"></div>
+</>
 }
 
 
