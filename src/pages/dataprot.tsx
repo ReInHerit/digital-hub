f8b34cfecd4820d01c2fa85475e3b50b88ea64fe
Sebi <SebastianStoff@gmx.at>
2021-11-14 18:38:21 +0100

    #34. Added grid where engageImage component is being displayed

--- src/pages/dataprot.tsx
+++ src/pages/dataprot.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import ImageEditor from "../components/shared/ImageEditor";
 import BaseLayout from "../components/static/BaseLayout"
 import EngageImage from "../components/shared/EngageImage";
+import { Col, Row } from "react-bootstrap";
 
 const DataProtectionPage = () => {
 
@@ -17,7 +18,23 @@ const DataProtectionPage = () => {
       
       <hr/>
 
-      <EngageImage imgAddress={url}></EngageImage>
+      <Row>
+        <Col md={5}>
+          <EngageImage imgAddress={url}></EngageImage>
+        </Col>
+        <Col md={5}>
+          <EngageImage imgAddress={"https://picsum.photos/id/220/600/400"}></EngageImage>
+        </Col>
+      </Row>
+      <br />
+      <Row>
+        <Col md={10}>
+        <EngageImage imgAddress={"https://picsum.photos/id/22/400/300"}></EngageImage>
+        </Col>
+      </Row>
+      <br />
+      <EngageImage imgAddress={"https://picsum.photos/id/12/1000/1000"}></EngageImage>
+      
 
     </BaseLayout>
   )
