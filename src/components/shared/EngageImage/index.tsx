6ecf27115a5263d8265ca35ed2a6942b6cdb74c4
Sebi <SebastianStoff@gmx.at>
2021-11-14 18:36:54 +0100

    #34. image editor may be called now via bootstrap modal

--- src/components/shared/EngageImage/index.tsx
+++ src/components/shared/EngageImage/index.tsx
@@ -1,26 +1,31 @@
 import React from "react"
 import ImageEditor from "../ImageEditor"
-import { Modal, Button } from "react-bootstrap"
+import { Modal, Button, Container, Card } from "react-bootstrap"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faToolbox } from "@fortawesome/free-solid-svg-icons"
 
 interface Props {
   imgAddress: string
 }
 
 const EngageImage: React.FC<Props> = props => {
-
   // modal code
-  const [show, setShow] = React.useState(false);
+  const [show, setShow] = React.useState(false)
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
 
   return (
     <>
-      <img src={props.imgAddress}></img>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
+      <Card>
+        <Card.Header>
+          <Button variant="secondary" onClick={handleShow}>
+            <FontAwesomeIcon icon={faToolbox}></FontAwesomeIcon>
+          </Button>{" "}
+          Example image heading...
+        </Card.Header>
+        <Card.Img variant="bottom" src={props.imgAddress} />
+      </Card>
 
       <Modal size="xl" show={show} onHide={handleClose} centered>
         <Modal.Header closeButton>
@@ -28,14 +33,11 @@ const EngageImage: React.FC<Props> = props => {
         </Modal.Header>
         <Modal.Body>
           Woohoo, you're reading this text in a modal!
-          <ImageEditor imgAddress={props.imgAddress}></ImageEditor>  
+          <ImageEditor imgAddress={props.imgAddress}></ImageEditor>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
           </Button>
         </Modal.Footer>
       </Modal>
