1665493d8d36f66fe3d0676374d6c87f47e99294
Sebi <SebastianStoff@gmx.at>
2021-11-15 10:32:32 +0100

    #20. added intern page

---
+++ src/pages/intern.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import BaseLayout from "../components/static/BaseLayout"
+
+
+const Intern = () => {
+
+
+
+  return <>
+    <BaseLayout>
+      <h1>Intern project area</h1>
+      <br />
+      <a href="/admin/admin.html" target="_blank">Link to Netlify CMS</a>
+    </BaseLayout>
+  </>
+}
+
+export default Intern;
