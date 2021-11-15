17d263c031c61a878333aaa75107a054e351c118
Sebi <SebastianStoff@gmx.at>
2021-11-15 10:35:49 +0100

    #20. added more dummy content

--- src/pages/intern.tsx
+++ src/pages/intern.tsx
@@ -2,13 +2,18 @@ import React from "react"
 import BaseLayout from "../components/static/BaseLayout"
 
 
+/**
+ * Intern area of project - e.g. access to netlify CMS etc.
+ * @returns 
+ */
 const Intern = () => {
 
 
 
   return <>
     <BaseLayout>
-      <h1>Intern project area</h1>
+      <h1>Internal area</h1>
+      <p>Intern area of project - e.g. access to netlify CMS etc. / manuals on how to add images on pinterest etc.</p>
       <br />
       <a href="/admin/admin.html" target="_blank">Link to Netlify CMS</a>
     </BaseLayout>
