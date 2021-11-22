4c4625d28ccd6d00ba9f2a3025dbb3e84226a59b
Sebastian Stoff <sebastianstoff@gmx.at>
2021-11-22 14:03:39 +0100

    #53. moved to own databasket component

---
+++ src/components/shared/Databasket/index.tsx
@@ -0,0 +1,44 @@
+import React from "react"
+import { Button, ListGroup } from "react-bootstrap"
+import { useReinLocalStorage } from "../../../hooks/useReinLocalStorage"
+import { ReinUtils } from "../../../utils/ReinUtil"
+
+/**
+ * Component handles rendering of saved content.
+ * @returns 
+ */
+const Databasket = () => {
+  const { retrieveCollection, toggleItem } = useReinLocalStorage()
+
+  const [collection, setCollection] = React.useState([])
+
+  /**
+   * Reads out local storage and sets state accordingly on startup.
+   */
+  React.useEffect(() => {
+    if (ReinUtils.checkSSR()) return
+    setCollection(retrieveCollection())
+  }, [])
+
+  /**
+   * Handles toggling of local storage and synchronizes it with the local state.
+   * @param obj
+   */
+  const handleToggle = obj => {
+    const newArr = toggleItem(obj)
+    setCollection(newArr)
+  }
+
+  return (
+    <ListGroup>
+      {collection.map(item => (
+        <ListGroup.Item>
+          <p>{item.value}</p>
+          <Button onClick={() => handleToggle(item)} variant="danger">-</Button>
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  )
+}
+
+export default Databasket
--- src/pages/collection.tsx
+++ src/pages/collection.tsx
@@ -1,39 +1,12 @@
 import React from "react";
+import Databasket from "../components/shared/Databasket";
 import BaseLayout from "../components/static/BaseLayout";
-import { useReinLocalStorage } from "../hooks/useReinLocalStorage";
-import { ReinUtils } from "../utils/ReinUtil";
-
 
 const CollectionPage = () => {
-
-  const {retrieveCollection, toggleItem} = useReinLocalStorage();
-
-
-  const [collection, setCollection] = React.useState([]);
-
-
-  /**
-   * Reads out local storage and sets state accordingly on startup.
-   */
-  React.useEffect(() => {
-    if(ReinUtils.checkSSR())return;
-    setCollection(retrieveCollection());
-  }, [])
-
-
-  /**
-   * Handles toggling of local storage and synchronizes it with the local state.
-   * @param obj 
-   */
-  const handleToggle = (obj) => {
-    const newArr = toggleItem(obj);
-    setCollection(newArr);
-  }
-
-
+  
   return <BaseLayout>
     <h2>Your Collection</h2>
-    <ul>{collection.map(item => <li onClick={() => handleToggle(item)}>{item.value}</li>)}</ul>
+    <Databasket></Databasket>
   
   </BaseLayout>
 
