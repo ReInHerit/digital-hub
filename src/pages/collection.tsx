4ebe526aa403eafd13e53c50360552c24c6cf483
Sebastian Stoff <sebastianstoff@gmx.at>
2021-11-22 12:11:22 +0100

    #53. added databasket demo inside collection page component

--- src/pages/collection.tsx
+++ src/pages/collection.tsx
@@ -1,17 +1,41 @@
 import React from "react";
 import BaseLayout from "../components/static/BaseLayout";
 import { useReinLocalStorage } from "../hooks/useReinLocalStorage";
+import { ReinUtils } from "../utils/ReinUtil";
 
 
 const CollectionPage = () => {
 
-  // const { collectionState, toggleItem } = useReinLocalStorage("none");
+  const {retrieveCollection, toggleItem} = useReinLocalStorage();
 
-  // return <BaseLayout>
-  //   <h2>Your Collection</h2>
-  //   <ul>{collectionState.map(item => <li onClick={() => toggleItem(item)}>{item.value}</li>)}</ul>
+
+  const [collection, setCollection] = React.useState([]);
+
+
+  /**
+   * Reads out local storage and sets state accordingly on startup.
+   */
+  React.useEffect(() => {
+    if(ReinUtils.checkSSR())return;
+    setCollection(retrieveCollection());
+  }, [])
+
+
+  /**
+   * Handles toggling of local storage and synchronizes it with the local state.
+   * @param obj 
+   */
+  const handleToggle = (obj) => {
+    const newArr = toggleItem(obj);
+    setCollection(newArr);
+  }
+
+
+  return <BaseLayout>
+    <h2>Your Collection</h2>
+    <ul>{collection.map(item => <li onClick={() => handleToggle(item)}>{item.value}</li>)}</ul>
   
-  // </BaseLayout>
+  </BaseLayout>
 
 }
 
