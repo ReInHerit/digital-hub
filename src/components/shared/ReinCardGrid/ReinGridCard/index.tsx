e843cc550a70ccadecb8ea38c7e35951b96ffdc6
Sebastian Stoff <sebastianstoff@gmx.at>
2021-11-23 12:39:04 +0100

    #58. added more options to ReinCard component

--- src/components/shared/ReinCardGrid/ReinGridCard/index.tsx
+++ src/components/shared/ReinCardGrid/ReinGridCard/index.tsx
@@ -5,13 +5,14 @@ import { Card, Col, Form } from "react-bootstrap";
 import { useReinLocalStorage } from "../../../../hooks/useReinLocalStorage";
 
 interface Props {
-  uid: string,
+  uid?: string,
   url: string,
   title: string,
   type: string,
   excerpt: string,
   targetAudience: string[]
-  faIcon: any
+  faIcon: any;
+  footerContent?: JSX.Element
 }
 
 /**
@@ -23,7 +24,7 @@ const ReinGridCard: React.FC<Props> = (props) => {
 
   const ReinStorage = useReinLocalStorage();
 
-  return <Col key={props.uid}>
+  return <Col key={props.uid && props.uid}>
     
       <Card className="shadow rounded border-light">
       <Link to={props.url} className="text-decoration-none text-dark">
@@ -39,12 +40,14 @@ const ReinGridCard: React.FC<Props> = (props) => {
         </Link>
         <Card.Footer className="bg-light border-0">
             <small className="text-muted d-inline">{props.targetAudience.map(aud => `${aud} `)}
-            <Form.Check
+            {props.uid && <Form.Check
               type="checkbox"
               label="(save to personal collection)"
               defaultChecked={ReinStorage.retrieveItem(props.uid) ? true : false}
               onClick={() => ReinStorage.toggleItem({value: false, id:props.uid, title: props.title, type:props.type as any})}
-            /></small>
+            />}
+            {props.footerContent && props.footerContent}
+            </small>
           </Card.Footer>
       </Card>
     
