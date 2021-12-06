656ceee134552702c78a84a1020eb902811557a3
Sebi <SebastianStoff@gmx.at>
2021-12-06 13:15:49 +0100

    #64. Added layout components

---
+++ src/components/shared/Layout/SideBarsLayout/index.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import { Col, Row } from "react-bootstrap"
+
+interface Props {
+  children: JSX.Element
+  left?: JSX.Element
+  right?: JSX.Element
+}
+
+/**
+ * Layout for navigation / documentation heavy pages.
+ * @param props 
+ * @returns 
+ */
+const SideBarsLayout: React.FC<Props> = props => {
+  return (
+    <Row>
+      <Col xl={1}>{props.left}</Col>
+      <Col>{props.children}</Col>
+      <Col xl={1}>{props.right}</Col>
+    </Row>
+  )
+}
+
+export default SideBarsLayout
