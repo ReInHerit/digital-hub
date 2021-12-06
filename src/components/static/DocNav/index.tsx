2d676c7c483fabbc3af493247b8a0f58f442b865
Sebi <SebastianStoff@gmx.at>
2021-12-06 13:23:01 +0100

    #64. Added docnav to components

---
+++ src/components/static/DocNav/index.tsx
@@ -0,0 +1,34 @@
+import { Link } from "gatsby";
+import React from "react";
+
+
+/**
+ * 
+ * @returns 
+ */
+const DocNav = () => {
+
+  return <ul>
+    <li>
+      <Link to="/doc">About</Link>
+    </li>
+    <li>
+      <Link to="/doc/architecture">Architecture</Link>
+    </li>
+    <li>
+      <Link to="/doc/datamodel">Datamodel</Link>
+    </li>
+    <li>
+      <Link to="/doc/reqs">Requirements</Link>
+    </li>
+    <li>
+      <Link to="/doc/workflows">Workflows</Link>
+    </li>
+  </ul>
+
+}
+
+
+export default DocNav;
+
+
--- src/pages/doc/index.tsx
+++ src/pages/doc/index.tsx
@@ -2,24 +2,13 @@ import { Link } from "gatsby"
 import React from "react"
 import SideBarsLayout from "../../components/shared/Layout/SideBarsLayout"
 import BaseLayout from "../../components/static/BaseLayout"
+import DocNav from "../../components/static/DocNav"
 
 const DocAbout = () => {
   return (
     <BaseLayout>
       <SideBarsLayout
-        left={
-          <>
-            <Link to="/doc">About</Link>
-            <br />
-            <Link to="/doc/architecture">Architecture</Link>
-            <br />
-            <Link to="/doc/datamodel">Datamodel</Link>
-            <br />
-            <Link to="/doc/reqs">Requirements</Link>
-            <br />
-            <Link to="/doc/workflows">Workflows</Link>
-          </>
-        }
+        left={<DocNav/>}
         right={<>Dummy Content</>}
       >
         <h1>Documentation about</h1>
