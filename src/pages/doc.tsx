511b4e166feae345df2e586b4b2b51ace4a73a65
Sebi <SebastianStoff@gmx.at>
2021-12-06 12:00:26 +0100

    #64. Added subnav for documentation page

--- src/pages/doc.tsx
+++ src/pages/doc.tsx
@@ -1,15 +1,22 @@
-import React from "react";
-import BaseLayout from "../components/static/BaseLayout";
-
+import { Link } from "gatsby"
+import React from "react"
+import BaseLayout from "../components/static/BaseLayout"
 
 const Documentation = () => {
-
-  return <BaseLayout>
-    <h1>Documentation page</h1>
-
-  </BaseLayout>
-
+  return (
+    <BaseLayout>
+      <h1>Documentation</h1>
+      <Link to="/doc/about">About</Link>
+      <br />
+      <Link to="/doc/about">Architecture</Link>
+      <br />
+      <Link to="/doc/about">Datamodel</Link>
+      <br />
+      <Link to="/doc/about">Requirements</Link>
+      <br />
+      <Link to="/doc/about">Workflows</Link>
+    </BaseLayout>
+  )
 }
 
-
-export default Documentation;
+export default Documentation
