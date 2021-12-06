38227f4b0a1704957bd22ef59e15c9e4c9ffb426
Sebi <SebastianStoff@gmx.at>
2021-12-06 13:17:08 +0100

    #64. using side layout component

--- src/pages/doc/index.tsx
+++ src/pages/doc/index.tsx
@@ -1,22 +1,31 @@
-import { Link } from "gatsby";
-import React from "react";
-import BaseLayout from "../../components/static/BaseLayout";
+import { Link } from "gatsby"
+import React from "react"
+import SideBarsLayout from "../../components/shared/Layout/SideBarsLayout"
+import BaseLayout from "../../components/static/BaseLayout"
 
 const DocAbout = () => {
-
-  return <BaseLayout>
-    <h1>Documentation about</h1>
-    <Link to="/doc">About</Link>
-      <br />
-      <Link to="/doc/architecture">Architecture</Link>
-      <br />
-      <Link to="/doc/datamodel">Datamodel</Link>
-      <br />
-      <Link to="/doc/reqs">Requirements</Link>
-      <br />
-      <Link to="/doc/workflows">Workflows</Link>
-  </BaseLayout>
-
+  return (
+    <BaseLayout>
+      <SideBarsLayout
+        left={
+          <>
+            <Link to="/doc">About</Link>
+            <br />
+            <Link to="/doc/architecture">Architecture</Link>
+            <br />
+            <Link to="/doc/datamodel">Datamodel</Link>
+            <br />
+            <Link to="/doc/reqs">Requirements</Link>
+            <br />
+            <Link to="/doc/workflows">Workflows</Link>
+          </>
+        }
+        right={<>Dummy Content</>}
+      >
+        <h1>Documentation about</h1>
+      </SideBarsLayout>
+    </BaseLayout>
+  )
 }
 
-export default DocAbout;
+export default DocAbout
