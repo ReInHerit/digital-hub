e936270f87a8939e5b7ea4e5c3e3f12ee6a9d23f
Sebi <SebastianStoff@gmx.at>
2021-12-07 10:31:44 +0100

    #64. added system context diagram to the page

--- src/pages/doc/architecture.tsx
+++ src/pages/doc/architecture.tsx
@@ -10,6 +10,21 @@ const Architecture = () => {
         left={<DocNav/>}
       >
         <h1>Software architecture of ReinHerit</h1>
+        <p>Praesent vel aliquet justo, sagittis egestas nunc.</p>
+        <br/>
+        <h2 className="h3">C4 diagram</h2>
+        <p>Context - Containers - Components - Code</p>
+        <p>uat tortor. Curabitur ornare ligula in ante accumsan, nec venenatis est luctus. Praesent vel aliquet justo, sagittis egestas nunc. Aliquam vitae felis blandit ex tempus facilisis. Nam imperdiet, lorem condimentum facilisis efficitur, felis justo commodo lorem, commodo tincidunt nisl felis nec quam.</p>
+        <p>uat tortor. Curabitur dnt vel aliquet justo, sagittis egestas nunc. Aliquam vitae felis blandit ex tempus facilisis. Nam imperdiet, lorem condimentum facilisis efficitur, felis justo commodo lorem, commodo tincidunt nisl felis nec quam.</p>
+        <br />
+        <br />
+        <h2 className="h3"><b>C</b>ontext: System context diagram</h2>
+        <p>Shows connection between ... Curabitur dnt vel aliquet justo, sagittis egestas nunc. Aliquam</p>
+        <br />
+        <a href="/images/diagrams/c4_system_context.jpg" target="_blank"><img src="/images/diagrams/c4_system_context.jpg" alt="c4 system context diagram" style={{maxWidth: "100%"}}></img></a>
+
+      
+      
       </SideBarsLayout>
       
     </BaseLayout>
