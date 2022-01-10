e7a35486b926c44a6d91f6805118234876bb1fb9
Sebastian Stoff <sebastianstoff@gmx.at>
2022-01-10 16:53:50 +0100

    #67. Added badge based filtering

--- src/components/shared/ToolkitFeed/index.tsx
+++ src/components/shared/ToolkitFeed/index.tsx
@@ -1,7 +1,7 @@
 import { faRss } from "@fortawesome/free-solid-svg-icons"
 import { graphql, useStaticQuery } from "gatsby"
 import React from "react"
-import { Form } from "react-bootstrap"
+import { Badge, Form } from "react-bootstrap"
 import SideMainLayout from "../Layout/SideMainLayout"
 import ReinCardGrid from "../ReinCardGrid"
 import ReinGridCard from "../ReinCardGrid/ReinGridCard"
@@ -11,55 +11,104 @@ import ReinGridCard from "../ReinCardGrid/ReinGridCard"
  * @returns
  */
 const ToolkitFeed: React.FC = () => {
-  const data: ToolsQueryData.Data = useStaticQuery(ToolsQuery);
+  const data: ToolsQueryData.Data = useStaticQuery(ToolsQuery)
 
-  const [tags, setTags] = React.useState([]);
+  const [tags, setTags] = React.useState([])
 
-  const ALL_TAGS = ["REST-API", "Library", "Web-Interface", "GraphQL-API", "Application", "CLI", "Python", "Java", "X-Technologies", "Javascript", "Typescript", "Web development", "Economics", "Cultural Heritage", "History", "Museology", "Machine Learning", "Topic Modeling", "NLP", "GIS", "Relational DB"]
+  const ALL_TAGS = [
+    "REST-API",
+    "Library",
+    "Web-Interface",
+    "GraphQL-API",
+    "Application",
+    "CLI",
+    "Python",
+    "Java",
+    "X-Technologies",
+    "Javascript",
+    "Typescript",
+    "Web development",
+    "Economics",
+    "Cultural Heritage",
+    "History",
+    "Museology",
+    "Machine Learning",
+    "Topic Modeling",
+    "NLP",
+    "GIS",
+    "Relational DB",
+  ]
 
-
-  const toggleTag = (evt: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
-    let tagsCopied = [...tags];
-    if( tags.includes(evt.currentTarget.value)){
-      let filtered = tagsCopied.filter((curTag) => evt.currentTarget.value !== curTag);
-      setTags(() => filtered);
+  const toggleTag = (toToggle: string) => {
+    let tagsCopied = [...tags]
+    if (tags.includes(toToggle)) {
+      let filtered = tagsCopied.filter(curTag => toToggle !== curTag)
+      setTags(() => filtered)
     } else {
-      tagsCopied.push(evt.currentTarget.value);
-      setTags(() => tagsCopied);
+      tagsCopied.push(toToggle)
+      setTags(() => tagsCopied)
     }
   }
 
   return (
     <SideMainLayout
-      side={<div className="bg-light p-4">
-        {ALL_TAGS.sort().map(curTag => <Form.Check type="checkbox" defaultChecked={false} key={curTag} onClick={toggleTag} label={curTag} value={curTag}></Form.Check>)}
-      </div>}
+      side={
+        <div className="bg-light p-4">
+          {ALL_TAGS.sort().map(curTag => (
+            <Form.Check
+              type="checkbox"
+              checked={tags.includes(curTag)}
+              defaultChecked={false}
+              key={curTag}
+              onClick={() => toggleTag(curTag)}
+              label={curTag}
+              value={curTag}
+            ></Form.Check>
+          ))}
+        </div>
+      }
     >
       <>
-      {tags.length !== 0 ? tags.join(" - ") : ' * '}
-      <hr></hr>
-      <ReinCardGrid>
-        {data.allMarkdownRemark.edges.map(edge => {
-          // filter only if no tags were selected
-          if( (tags.length !== 0) && !tags.some(curTag => edge.node.frontmatter.tool_type.includes(curTag))){ 
-            return;
-          } else {
-            return <ReinGridCard
-            key={edge.node.fields.typeCountId}
-            excerpt={edge.node.excerpt}
-            faIcon={faRss}
-            targetAudience={edge.node.frontmatter.target_audience}
-            title={edge.node.frontmatter.title}
-            type="news"
-            url={`/content/toolkits/${edge.node.fields.typeCountId}?mode=${""}`}
-            uid={edge.node.fields.typeCountId}
-          >
-            <span>{edge.node.fields.typeCountId}</span>
-          </ReinGridCard>
-          }
-          }
-        )}
-      </ReinCardGrid>
+        {tags.length !== 0
+          ? tags.map(tag => (
+              <Badge onClick={() => toggleTag(tag)} className="me-1 secondary">
+                {tag}
+              </Badge>
+            ))
+          : <Badge className="me-1 light">
+          *
+        </Badge>}
+        <hr></hr>
+        <ReinCardGrid>
+          {data.allMarkdownRemark.edges.map(edge => {
+            // filter only if no tags were selected
+            if (
+              tags.length !== 0 &&
+              !tags.some(curTag =>
+                edge.node.frontmatter.tool_type.includes(curTag)
+              )
+            ) {
+              return
+            } else {
+              return (
+                <ReinGridCard
+                  key={edge.node.fields.typeCountId}
+                  excerpt={edge.node.excerpt}
+                  faIcon={faRss}
+                  targetAudience={edge.node.frontmatter.target_audience}
+                  title={edge.node.frontmatter.title}
+                  type="news"
+                  url={`/content/toolkits/${
+                    edge.node.fields.typeCountId
+                  }?mode=${""}`}
+                  uid={edge.node.fields.typeCountId}
+                >
+                  <span>{edge.node.fields.typeCountId}</span>
+                </ReinGridCard>
+              )
+            }
+          })}
+        </ReinCardGrid>
       </>
     </SideMainLayout>
   )
@@ -70,7 +119,7 @@ export default ToolkitFeed
 const ToolsQuery = graphql`
   query ToolsQuery {
     allMarkdownRemark(
-      filter: {fileAbsolutePath: {regex: "/tools/"}, frontmatter: {}}
+      filter: { fileAbsolutePath: { regex: "/tools/" }, frontmatter: {} }
     ) {
       edges {
         node {
@@ -94,54 +143,48 @@ const ToolsQuery = graphql`
       }
     }
   }
-
 `
 
-
 declare module ToolsQueryData {
-
   export interface Fields {
-      typeCountId: string;
+    typeCountId: string
   }
 
   export interface Frontmatter {
-      title: string;
-      date: string;
-      target_audience: string[];
-      layout?: any;
-      author?: any;
-      training_available: boolean;
-      tool_type?: any;
-      type: string;
+    title: string
+    date: string
+    target_audience: string[]
+    layout?: any
+    author?: any
+    training_available: boolean
+    tool_type?: any
+    type: string
   }
 
   export interface Node {
-      id: string;
-      html: string;
-      excerpt: string;
-      fields: Fields;
-      frontmatter: Frontmatter;
+    id: string
+    html: string
+    excerpt: string
+    fields: Fields
+    frontmatter: Frontmatter
   }
 
   export interface Edge {
-      node: Node;
+    node: Node
   }
 
   export interface AllMarkdownRemark {
-      edges: Edge[];
+    edges: Edge[]
   }
 
   export interface Data {
-      allMarkdownRemark: AllMarkdownRemark;
+    allMarkdownRemark: AllMarkdownRemark
   }
 
-  export interface Extensions {
-  }
+  export interface Extensions {}
 
   export interface RootObject {
-      data: Data;
-      extensions: Extensions;
+    data: Data
+    extensions: Extensions
   }
-
 }
-
