8e213d8d34f44313d37a0f7cd556157c807ed45c
Sebastian Stoff <sebastianstoff@gmx.at>
2022-01-10 15:35:50 +0100

    #67. removed unnecessary imports

--- src/pages/toolkit.tsx
+++ src/pages/toolkit.tsx
@@ -1,8 +1,4 @@
-import { faDeaf, faTools } from "@fortawesome/free-solid-svg-icons"
 import React from "react"
-import SideMainLayout from "../components/shared/Layout/SideMainLayout"
-import ReinCardGrid from "../components/shared/ReinCardGrid"
-import ReinGridCard from "../components/shared/ReinCardGrid/ReinGridCard"
 import ToolkitFeed from "../components/shared/ToolkitFeed"
 import BaseLayout from "../components/static/BaseLayout"
 
