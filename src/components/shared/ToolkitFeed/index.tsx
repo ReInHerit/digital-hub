06d5276fc3dd7005b82bfedfa4300b92cbef7a9b
Sebastian Stoff <sebastianstoff@gmx.at>
2022-01-10 15:35:18 +0100

    #67. moved side main to toolkitfeed comp

--- src/components/shared/ToolkitFeed/index.tsx
+++ src/components/shared/ToolkitFeed/index.tsx
@@ -1,6 +1,7 @@
 import { faRss } from "@fortawesome/free-solid-svg-icons"
 import { graphql, useStaticQuery } from "gatsby"
 import React from "react"
+import SideMainLayout from "../Layout/SideMainLayout"
 import ReinCardGrid from "../ReinCardGrid"
 import ReinGridCard from "../ReinCardGrid/ReinGridCard"
 
@@ -9,25 +10,36 @@ import ReinGridCard from "../ReinCardGrid/ReinGridCard"
  * @returns
  */
 const ToolkitFeed: React.FC = () => {
-  const data: ToolsQueryData.Data = useStaticQuery(ToolsQuery)
+  const data: ToolsQueryData.Data = useStaticQuery(ToolsQuery);
+
+  const [tags, setTags] = React.useState();
+
+  const ALL_TAGS = ["REST-API", "Library", "Web-Interface", "GraphQL-API", "Application", "CLI", "Python", "Java", "X-Technologies", "Javascript", "Typescript", "Web development", "Economics", "Cultural Heritage", "History", "Museology", "Machine Learning", "Topic Modeling", "NLP", "GIS", "Relational DB"]
+
 
   return (
-    <ReinCardGrid>
-      {data.allMarkdownRemark.edges.map(edge => (
-        <ReinGridCard
-          key={edge.node.fields.typeCountId}
-          excerpt={edge.node.excerpt}
-          faIcon={faRss}
-          targetAudience={edge.node.frontmatter.target_audience}
-          title={edge.node.frontmatter.title}
-          type="news"
-          url={`/content/toolkits/${edge.node.fields.typeCountId}?mode=${""}`}
-          uid={edge.node.fields.typeCountId}
-        >
-          <span>{edge.node.fields.typeCountId}</span>
-        </ReinGridCard>
-      ))}
-    </ReinCardGrid>
+    <SideMainLayout
+      side={<ul>
+        {ALL_TAGS.map(curTag => <li>{curTag}</li>)}
+      </ul>}
+    >
+      <ReinCardGrid>
+        {data.allMarkdownRemark.edges.map(edge => (
+          <ReinGridCard
+            key={edge.node.fields.typeCountId}
+            excerpt={edge.node.excerpt}
+            faIcon={faRss}
+            targetAudience={edge.node.frontmatter.target_audience}
+            title={edge.node.frontmatter.title}
+            type="news"
+            url={`/content/toolkits/${edge.node.fields.typeCountId}?mode=${""}`}
+            uid={edge.node.fields.typeCountId}
+          >
+            <span>{edge.node.fields.typeCountId}</span>
+          </ReinGridCard>
+        ))}
+      </ReinCardGrid>
+    </SideMainLayout>
   )
 }
 
