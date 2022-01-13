281a5513ebbb750b40395bd751859cd7c839e2c4
Sebastian Stoff <sebastianstoff@gmx.at>
2022-01-13 13:28:34 +0100

    #69. changed label of portal page

--- src/components/shared/PortalContent/index.tsx
+++ src/components/shared/PortalContent/index.tsx
@@ -20,7 +20,7 @@ const PortalContent = () => {
         <Breadcrumb.Item><Link to={`/?mode=${theme.mode}`}>Home</Link></Breadcrumb.Item>
         <Breadcrumb.Item>{ reinheritStatics[theme.groupKey].LABEL} Portal</Breadcrumb.Item>
       </Breadcrumb>
-      <h1 style={{color: theme.styles.MAIN_COLOR}}>{reinheritStatics[theme.groupKey].LABEL}</h1>
+      <h1 style={{color: theme.styles.MAIN_COLOR}}>Portal</h1>
       
       <ReinCardGrid>
         <ReinGridCard
