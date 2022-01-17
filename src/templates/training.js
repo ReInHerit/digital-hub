cc9ede24b864c6dcb4353d084c47a74959245b6a
Sebi <SebastianStoff@gmx.at>
2022-01-17 13:40:41 +0100

    #73. updated metadarta box a bitr

--- src/templates/training.js
+++ src/templates/training.js
@@ -5,6 +5,7 @@ import { Col, Container, Row } from "react-bootstrap"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faArrowAltCircleLeft } from "@fortawesome/free-solid-svg-icons"
 import { useReinLocalStorage } from "../hooks/useReinLocalStorage"
+import SideBarsLayout from "../components/shared/Layout/SideBarsLayout"
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -12,48 +13,72 @@ export default function Template({
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
 
-  const { mapFaSymbol } = useReinLocalStorage();
+  const { mapFaSymbol } = useReinLocalStorage()
 
   return (
     <BaseLayout>
       <div className="blog-post-container">
         <div className="blog-post">
-          
-          <h1 className="h2">{frontmatter.title} <FontAwesomeIcon color="lightgrey" icon={mapFaSymbol(frontmatter.type)}></FontAwesomeIcon></h1>
-          <p><FontAwesomeIcon color="lightgrey" icon={faArrowAltCircleLeft} /> <Link to={`/${frontmatter.type}`}>{frontmatter.type.charAt(0).toUpperCase() + frontmatter.type.slice(1)} overview</Link></p>
-          <br/>
-          <Row>
-            <Col lg={2}>
-              {/* {markdownRemark.id} */}
-              <div><b className="text-secondary">Author(s)</b></div>
-              <div>{markdownRemark.author ? markdownRemark.author : "ReInHerit project team"}</div>
-              <br/>
+          <h1 className="h2">
+            {frontmatter.title}{" "}
+            <FontAwesomeIcon
+              color="lightgrey"
+              icon={mapFaSymbol(frontmatter.type)}
+            ></FontAwesomeIcon>
+          </h1>
+          <p>
+            <FontAwesomeIcon color="lightgrey" icon={faArrowAltCircleLeft} />{" "}
+            <Link to={`/${frontmatter.type}`}>
+              {frontmatter.type.charAt(0).toUpperCase() +
+                frontmatter.type.slice(1)}{" "}
+              overview
+            </Link>
+          </p>
+          <br />
 
-              <div><b className="text-secondary">Publication date</b></div>
-              <div>{frontmatter.date}</div>
-              <br/>
+          <SideBarsLayout
+            left={
+              <div className="reincard card p-4">
+                <div>
+                  <b className="text-secondary">Author(s)</b>
+                </div>
+                <div>
+                  {markdownRemark.author
+                    ? markdownRemark.author
+                    : "ReInHerit project team"}
+                </div>
+                <br />
 
-              <div><b className="text-secondary">Cite as</b></div>
-              <div>"Mustermann, Max; Doing development, ... "</div>
-              <br/>
+                <div>
+                  <b className="text-secondary">Publication date</b>
+                </div>
+                <div>{frontmatter.date}</div>
+                <br />
 
-              <div><b className="text-secondary">Terms of reuse</b></div>
-              <div>MIT - CC BY 4.0</div>
-              <br/>
-            </Col>
-            <Col lg={10}>
-              <Container fluid className="bg-white rounded shadow p-4">
-                <div
-                  className="blog-post-content"
-                  dangerouslySetInnerHTML={{ __html: html }}
-                />
-              </Container>
-            </Col>
-          </Row>
+                <div>
+                  <b className="text-secondary">Cite as</b>
+                </div>
+                <div>"Mustermann, Max; Doing development, ... "</div>
+                <br />
+
+                <div>
+                  <b className="text-secondary">Terms of reuse</b>
+                </div>
+                <div>MIT - CC BY 4.0</div>
+                <br />
+              </div>
+            }
+          >
+            <Container fluid className="bg-white">
+              <div
+                className="blog-post-content"
+                dangerouslySetInnerHTML={{ __html: html }}
+              />
+            </Container>
+          </SideBarsLayout>
         </div>
-    </div>
+      </div>
     </BaseLayout>
-    
   )
 }
 export const pageQuery = graphql`
@@ -70,4 +95,5 @@ export const pageQuery = graphql`
       }
       id
     }
-  }`
+  }
+`
