672b4fe63e233251103987acd449b1e61cce2af9
Sebastian Stoff <sebastianstoff@gmx.at>
2022-01-31 18:37:16 +0100

    #76. added skeleton designs page

---
+++ src/pages/designs/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import BaseLayout from "../../components/static/BaseLayout"
+
+const Designs = () => {
+  return (
+    <BaseLayout>
+      <h2>Designs</h2>
+    </BaseLayout>
+  )
+}
+
+export default Designs
