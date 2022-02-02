7ba8be2889c8f365bb743f0332ae1a9ce565f328
Sebi <SebastianStoff@gmx.at>
2022-02-02 10:14:41 +0100

    #80. Added prototypical card overview of available designs in the hub

--- src/pages/designs/index.tsx
+++ src/pages/designs/index.tsx
@@ -1,18 +1,50 @@
 import { faObjectUngroup } from "@fortawesome/free-regular-svg-icons"
 import { faTools } from "@fortawesome/free-solid-svg-icons"
 import React from "react"
+import { Badge, Form } from "react-bootstrap"
 import SideMainLayout from "../../components/shared/Layout/SideMainLayout"
 import ReinCardGrid from "../../components/shared/ReinCardGrid"
 import ReinGridCard from "../../components/shared/ReinCardGrid/ReinGridCard"
 import BaseLayout from "../../components/static/BaseLayout"
 
 const Designs = () => {
+
+  const tags = ["portrait", "landscape", "etc."]
+
   return (
     <BaseLayout>
       <h2>Designs</h2>
+      <p>Available via the hub</p>
 
-      <SideMainLayout>
-      <ReinCardGrid>
+      <SideMainLayout
+        side={<div className="border p-4 reincard">
+        Filter categories
+        <br/>
+        {tags.length !== 0
+        ? tags.map(tag => (
+            <Badge bg="white" color="black" text={"black" as "white"} className="me-1 btn border">
+              {tag}
+            </Badge>
+          ))
+        : <Badge className="me-1 btn border" bg="white" color="black" text={"black" as "white"}>
+        *
+      </Badge>}
+      <br></br>
+      <br></br>
+        {tags.sort().map(curTag => (
+          <Form.Check
+            type="checkbox"
+            // checked={tags.includes(curTag)}
+            defaultChecked={false}
+            key={curTag}
+            // onClick={() => toggleTag(curTag)}
+            label={curTag}
+            value={curTag}
+          ></Form.Check>
+        ))}
+      </div>}
+      >
+        <ReinCardGrid>
           <ReinGridCard
             key={"edge.node.fields.typeCountId"}
             excerpt={"edge.node.excerpt"}
@@ -63,10 +95,8 @@ const Designs = () => {
             url={`google.com`}
             uid="123233"
           ></ReinGridCard>
-              
         </ReinCardGrid>
       </SideMainLayout>
-
     </BaseLayout>
   )
 }
