fd88d0f1d72843cfed7053bd8801a8fdc66ff2bd
Sebi <SebastianStoff@gmx.at>
2022-02-09 13:06:46 +0100

    #86. removed outdated code

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -11,7 +11,7 @@ const roles = [
   "#Make it your own",
   "#Redefine #makeityourown",
   "#Teamwork",
-  "#Aiming at international cooperation and networking for cultural heritage actors"
+  "#Aiming at international cooperation and networking for cultural heritage actors",
 ]
 const Hero = () => {
   return (
@@ -36,9 +36,6 @@ const Hero = () => {
             the tools and events developed in the course of the ReInherit
             project.
           </p>
-          {/* <p>Are you ready to see what the future of cultural heritage may hold?</p> */}
-          {/* <p>Then .. </p> */}
-          {/* <p className="" style={{color:"rgba(52 ,  166,  194, 1)", fontWeight:600, fontSize:"1.5em"}}>Choose your Path and Make it Your Own! </p> */}
           <Typed
             loop
             typeSpeed={10}
@@ -63,12 +60,6 @@ const Hero = () => {
             color="lightgrey"
             icon={faChevronDown}
           ></FontAwesomeIcon>
-        </Col>
-        <Col lg={6}>
-          <div style={{ width: "100%" }}>
-            {/* <Image alt="Virtual Healthcare for you" filename="rein_logo.jpg" style={{maxWidth: "40%"}}/> */}
-            {/* <Image alt="Virtual Healthcare for you" filename="rein_eu.png" style={{maxWidth: "40%"}}/> */}
-          </div>
         </Col>
       </Row>
     </div>
