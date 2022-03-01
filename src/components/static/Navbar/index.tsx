74e8df79216ce23a1b93a9538b1f406e641521f9
Sebastian Stoff <sebastianstoff@gmx.at>
2022-03-01 14:37:18 +0100

    #99. fixed last navbar parts

--- src/components/static/Navbar/index.tsx
+++ src/components/static/Navbar/index.tsx
@@ -21,7 +21,6 @@ const ReinNavbar: React.FC = () => {
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
               <Link className="text-dark text-decoration-none nav-link" to={addMode("/", theme.mode)}>Digital Hub</Link>
-              {/* <Nav.Link as="div"><Link className="text-dark text-decoration-none" to={addMode("/", theme.mode)}>Digital Hub</Link></Nav.Link> */}
               <NavDropdown title="Project" id="basic-nav-dropdown">
                 <Link className="text-dark text-decoration-none dropdown-item" to="/about">About</Link>
                 <Link className="text-dark text-decoration-none dropdown-item" to="/team">Team</Link>
@@ -35,34 +34,23 @@ const ReinNavbar: React.FC = () => {
                 <Link className="text-dark text-decoration-none dropdown-item" to="/exhibs/digital">Digital Exhibition</Link>
                 <Link className="text-dark text-decoration-none dropdown-item" to="/exhibs/travel">Traveling Exhibition</Link>
               </NavDropdown>
+
+              <Link className="text-dark text-decoration-none nav-link" to={addMode("/tools", theme.mode)}>Toolkit</Link>
               
-              <Nav.Link as="div"><Link to="/tools" className="text-dark text-decoration-none">Toolkit</Link></Nav.Link>
-              
-              {/* <NavDropdown title="Toolkit" id="nav_toolkit">
-                <NavDropdown.Item as="div" href="#action/3.2">
-                  Apps
-                </NavDropdown.Item>
-                <NavDropdown.Item as="div" href="#action/3.3">
-                  Game(s)
-                </NavDropdown.Item>
-                <NavDropdown.Item as="div" href="#action/3.3">
-                <Link to={addMode("/content/training", theme.mode)} className="text-dark text-decoration-none">Training / Webinars</Link>
-                </NavDropdown.Item>
-              </NavDropdown> */}
               <NavDropdown title="Performances" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.0"><Link to="/portal/performances" className="text-dark text-decoration-none">Overview</Link></NavDropdown.Item>
+                <Link className="text-dark text-decoration-none dropdown-item" to="/portal/performances">Overview</Link>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.1"><Link to="/content/performances/perf1" className="text-dark text-decoration-none">Performance 1</Link></NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.1"><Link to="/content/performances/perf2" className="text-dark text-decoration-none">Performance 2</Link></NavDropdown.Item>
+                <Link className="text-dark text-decoration-none dropdown-item" to="/content/performances/perf1">Performance 1</Link>
+                <Link className="text-dark text-decoration-none dropdown-item" to="/content/performances/perf2">Performance 2</Link>
               </NavDropdown>
-              
-              <Nav.Link as="div"><Link to="/designs" className="text-dark text-decoration-none">E-Shop</Link></Nav.Link>
+
+              <Link className="text-dark text-decoration-none nav-link" to={addMode("/designs", theme.mode)}>E-shop</Link>
             </Nav>
             <Nav>
             <Form.Select className="border-0" aria-label="Default select example" onChange={(evt) => theme.changeTheme(evt.currentTarget.value as any)}>
               {Object.entries(reinheritStatics).map(([key, propVal]) => <option key={`restmode_select_${propVal.REST_MODE_VAL}`} selected={theme.mode === propVal.REST_MODE_VAL} value={propVal.REST_MODE_VAL}>{propVal.LABEL}</option>)}
             </Form.Select>
-              <Nav.Link as="div"><Link to={addMode("/portal", theme.mode)} className="text-dark text-decoration-none">Portal</Link></Nav.Link>
+              <Link className="text-dark text-decoration-none nav-link" to={addMode("/portal", theme.mode)}>Portal</Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
