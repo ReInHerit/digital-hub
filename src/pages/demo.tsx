35a14d92a41ebddfb1cd3b936af1f16d51c88468
Sebastian Stoff <sebastianstoff@gmx.at>
2022-05-09 14:14:12 +0200

    #130. created own demo page with forum display

---
+++ src/pages/demo.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import BaseLayout from "../components/static/BaseLayout";
+import Forum from "../components/shared/Forum";
+
+const Demo = () => {
+
+  return (
+    <BaseLayout>
+      <h1>Demo / show case page for the digital hub</h1>
+      <br></br>
+      <br></br>
+      <br></br>
+      <Forum></Forum>
+    </BaseLayout>
+  )
+
+}
+
+
+export default Demo;
