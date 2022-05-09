499e55a0cb78f07344215310285cebf565c83d65
Sebastian Stoff <sebastianstoff@gmx.at>
2022-05-09 14:14:31 +0200

    #130. removed forum display from entry page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import Hero from "../components/hero"
 import BaseLayout from "../components/static/BaseLayout"
 import NetlifyCMSAuth from "../components/shared/NetlifyCMSAdminCDN/NetlifyCMSAuth"
 import Landing from "../components/static/Landing"
-import Forum from "../components/shared/Forum"
 
 const IndexPage = () => (
   <React.Fragment>
@@ -12,7 +11,6 @@ const IndexPage = () => (
         <SEO title="Home" />
           <Hero />
           <Landing></Landing>
-          <Forum></Forum>
           {/* Necessary for users to allow seting a pw after getting the invite mail  */}
           <NetlifyCMSAuth></NetlifyCMSAuth>
       </BaseLayout>
