33d46e0394a6ccfeb969bd9f0914ab1b452db3b8
Sebastian Stoff <sebastianstoff@gmx.at>
2022-05-09 13:40:54 +0200

    #129. integrated forum by test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Hero from "../components/hero"
 import BaseLayout from "../components/static/BaseLayout"
 import NetlifyCMSAuth from "../components/shared/NetlifyCMSAdminCDN/NetlifyCMSAuth"
 import Landing from "../components/static/Landing"
+import Forum from "../components/shared/Forum"
 
 const IndexPage = () => (
   <React.Fragment>
@@ -11,6 +12,7 @@ const IndexPage = () => (
         <SEO title="Home" />
           <Hero />
           <Landing></Landing>
+          <Forum></Forum>
           {/* Necessary for users to allow seting a pw after getting the invite mail  */}
           <NetlifyCMSAuth></NetlifyCMSAuth>
       </BaseLayout>
