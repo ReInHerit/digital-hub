1a0e76e89d54bfa4b61f540c0908248c9bb070e9
Sebi <SebastianStoff@gmx.at>
2022-05-19 16:54:21 +0200

    #134. removed now unneeded landing component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,12 @@ import SEO from "../components/seo"
 import Hero from "../components/hero"
 import BaseLayout from "../components/static/BaseLayout"
 import NetlifyCMSAuth from "../components/shared/NetlifyCMSAdminCDN/NetlifyCMSAuth"
-import Landing from "../components/static/Landing"
 
 const IndexPage = () => (
   <React.Fragment>
       <BaseLayout>
         <SEO title="Home" />
           <Hero />
-          {/* <Landing></Landing> */}
           {/* Necessary for users to allow seting a pw after getting the invite mail  */}
           <NetlifyCMSAuth></NetlifyCMSAuth>
       </BaseLayout>
