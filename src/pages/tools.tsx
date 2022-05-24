cf82f19783158b8423faa2a96ad66a0fcaa8c1a8
Sebastian Stoff <sebastianstoff@gmx.at>
2022-05-24 08:36:28 +0200

    #138. added spacing for responsiveness

--- src/pages/tools.tsx
+++ src/pages/tools.tsx
@@ -10,6 +10,7 @@ const Toolkit = () => {
         subHeading="Tools available via the ReInHerit toolkit."
       >ReInHerit Toolkit</MainHeading>
       <p className="w-75">All tools on the platform are <b style={{color:"#d7487e"}}>categorized</b> and public available. </p>
+      <br></br>
       <ToolkitFeed></ToolkitFeed>
     </BaseLayout>
   )
