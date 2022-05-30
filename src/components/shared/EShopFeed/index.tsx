5a8bf617f84472de50144b2b90e4910ce392a3ce
Sebi <SebastianStoff@gmx.at>
2022-05-30 15:20:26 +0200

    #144. added thumbnials to eshop item overview

--- src/components/shared/EShopFeed/index.tsx
+++ src/components/shared/EShopFeed/index.tsx
@@ -1,44 +1,41 @@
-import { faImage } from "@fortawesome/free-solid-svg-icons";
-import { graphql, useStaticQuery } from "gatsby";
-import React from "react";
-import ReinCardGrid from "../ReinCardGrid";
-import ReinGridCard from "../ReinCardGrid/ReinGridCard";
-
+import { faImage } from "@fortawesome/free-solid-svg-icons"
+import { graphql, useStaticQuery } from "gatsby"
+import React from "react"
+import ReinCardGrid from "../ReinCardGrid"
+import ReinGridCard from "../ReinCardGrid/ReinGridCard"
 
 /**
  * Generated overview over eshop items
- * @returns 
+ * @returns
  */
 const EShopFeed: React.FC = () => {
+  const data: EshopQueryData.Data = useStaticQuery(EshopQuery)
 
-  const data: EshopQueryData.Data = useStaticQuery(EshopQuery);
-
-
-
-  return <ReinCardGrid>
-    {data.allMarkdownRemark.edges.map(edge => <ReinGridCard
-    
-      excerpt={edge.node.excerpt}
-      faIcon={faImage}
-      targetAudience={["VISITOR"]}
-      title={edge.node.frontmatter.title}
-      type="eshop"
-      url={`/eshop/${edge.node.id}`}
-      uid={edge.node.id}
-    
-    />)}
-  </ReinCardGrid>
+  return (
+    <ReinCardGrid>
+      {data.allMarkdownRemark.edges.map(edge => (
+        <ReinGridCard
+          excerpt={edge.node.excerpt}
+          faIcon={faImage}
+          targetAudience={["VISITOR"]}
+          title={edge.node.frontmatter.title}
+          type="eshop"
+          url={`/eshop/${edge.node.id}`}
+          uid={edge.node.id}
+        >
+          <img style={{maxWidth:200}} src={edge.node.frontmatter.thumbnail}></img>
+        </ReinGridCard>
+      ))}
+    </ReinCardGrid>
+  )
 }
 
-
-export default EShopFeed;
-
-
+export default EShopFeed
 
 const EshopQuery = graphql`
   query EshopQuery {
     allMarkdownRemark(
-      filter: {fileAbsolutePath: {regex: "/eshop/"}, frontmatter: {}}
+      filter: { fileAbsolutePath: { regex: "/eshop/" }, frontmatter: {} }
     ) {
       edges {
         node {
@@ -54,40 +51,34 @@ const EshopQuery = graphql`
   }
 `
 
-
-
 declare module EshopQueryData {
-
   export interface Frontmatter {
-      title: string;
-      thumbnail: string;
+    title: string
+    thumbnail: string
   }
 
   export interface Node {
-      id: string;
-      excerpt: string;
-      frontmatter: Frontmatter;
+    id: string
+    excerpt: string
+    frontmatter: Frontmatter
   }
 
   export interface Edge {
-      node: Node;
+    node: Node
   }
 
   export interface AllMarkdownRemark {
-      edges: Edge[];
+    edges: Edge[]
   }
 
   export interface Data {
-      allMarkdownRemark: AllMarkdownRemark;
+    allMarkdownRemark: AllMarkdownRemark
   }
 
-  export interface Extensions {
-  }
+  export interface Extensions {}
 
   export interface RootObject {
-      data: Data;
-      extensions: Extensions;
+    data: Data
+    extensions: Extensions
   }
-
 }
-
