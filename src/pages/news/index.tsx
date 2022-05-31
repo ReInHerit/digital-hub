2c5a77a8b5b9e65daf2abbac5145667bc83df4d9
Sebi <SebastianStoff@gmx.at>
2022-05-31 13:28:01 +0200

    #138. removed side main layout at news

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -12,18 +12,7 @@ const NewsPage: React.FC = () => {
         News
       </MainHeading>
       <p>Here you'll find <b style={{color:"#2eacc8"}}>explicit news</b> messages provided by the ReInHerit team. This page is not meant as display of recent activity</p>
-      <br />
-      <SideMainLayout
-        side={
-          <div className="p-4 border border-dark reincard">
-            <p><b style={{color:"#d7487e"}}>Browse through</b> the hub's news</p>
-            <p>All news relevant for Reinherit ... </p>
-            <p>Scroll down / click on card etc. ... </p>
-          </div>
-        }
-      >
-        <NewsFeed />
-      </SideMainLayout>
+      <NewsFeed></NewsFeed>
     </BaseLayout>
   )
 }
