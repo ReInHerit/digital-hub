2bbf301fc84b94c3f768ca97ec04024646edfa8b
Sebastian Stoff <sebastianstoff@gmx.at>
2022-06-01 15:07:02 +0200

    #143. improved webinar overview page

--- src/pages/webinars/index.tsx
+++ src/pages/webinars/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import BaseLayout from "../../components/static/BaseLayout";
+import WebinarFeed from "../../components/shared/WebinarFeed";
+import MainHeading from "../../components/static/MainHeading";
 
 const WebinarsPage = () => {
 
   return <BaseLayout
   >
-    Webinars Page
+    <MainHeading
+      subHeading="Webinars provided by the digital hub"
+    >Webinars</MainHeading>
+    <WebinarFeed></WebinarFeed>
   </BaseLayout>
 
 }
