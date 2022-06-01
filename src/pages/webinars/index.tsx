86e45d1236e9a4d9f198abc110fe10c3e7671384
Sebi <SebastianStoff@gmx.at>
2022-06-01 13:04:05 +0200

    #143. added base webinars overview page

---
+++ src/pages/webinars/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BaseLayout from "../../components/static/BaseLayout";
+
+const WebinarsPage = () => {
+
+  return <BaseLayout
+  >
+    Webinars Page
+  </BaseLayout>
+
+}
+
+
+export default WebinarsPage;
