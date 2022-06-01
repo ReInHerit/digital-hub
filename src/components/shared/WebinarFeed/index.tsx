afc30ee8dbdc7f4673d0cb831a256d2c55d0c796
Sebastian Stoff <sebastianstoff@gmx.at>
2022-06-01 15:09:43 +0200

    #143. removed unnecessary side layout

--- src/components/shared/WebinarFeed/index.tsx
+++ src/components/shared/WebinarFeed/index.tsx
@@ -9,54 +9,17 @@ const TrainingFeed: React.FC = () => {
   const data: TrainingModel.Data = useStaticQuery(TRAINING_QUERY)
 
   return (
-    <>
-      <SideMainLayout
-        side={
-          <>
-            <div>
-              <b className="text-secondary">Summary</b>
-            </div>
-            <p>
-              Here you'll find a small overview ... oids pleasure itself,
-              because it is pleasure, but because those who do not know how to
-              pursue pleasure rationally encounter ...
-            </p>
-            <div>
-              <b className="text-secondary">Content</b>
-            </div>
-            <ul className="m-0 p-1 pt-0" style={{ listStyle: "none" }}>
-              {data.allMarkdownRemark.edges.map(edge => (
-                <li className="p-0 m-0">
-                  <small>
-                    <Link
-                      to={`/webinars/${edge.node.frontmatter.pageId}`}
-                      className="text-dark"
-                    >
-                      {edge.node.frontmatter.title}
-                    </Link>{" "}
-                    <span className="text-muted">
-                      ({edge.node.frontmatter.date})
-                    </span>
-                  </small>
-                </li>
-              ))}
-            </ul>
-          </>
-        }
-      >
-        <ReinCardGrid>
-          {data.allMarkdownRemark.edges.map((training, i) => (
-            <ReinGridCard
-              key={training.node.frontmatter.pageId}
-              url={`/webinars/${training.node.frontmatter.pageId}`}
-              title={training.node.frontmatter.title}
-              body={training.node.excerpt}
-              uid={training.node.frontmatter.pageId}
-            ></ReinGridCard>
-          ))}
-        </ReinCardGrid>
-      </SideMainLayout>
-    </>
+    <ReinCardGrid>
+      {data.allMarkdownRemark.edges.map((training, i) => (
+        <ReinGridCard
+          key={training.node.frontmatter.pageId}
+          url={`/webinars/${training.node.frontmatter.pageId}`}
+          title={training.node.frontmatter.title}
+          body={training.node.excerpt}
+          uid={training.node.frontmatter.pageId}
+        ></ReinGridCard>
+      ))}
+    </ReinCardGrid>
   )
 }
 
@@ -92,7 +55,7 @@ declare module TrainingModel {
   export interface Node {
     html: string
     excerpt: string
-    frontmatter: Frontmatter,
+    frontmatter: Frontmatter
   }
 
   export interface Edge {
