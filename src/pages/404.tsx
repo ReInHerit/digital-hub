b6a7f0dc550a8202b65700739e55365d95494fae
Sebi <SebastianStoff@gmx.at>
2022-06-14 16:44:41 +0200

    #177. enhanced coming soon page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,5 @@
+import { faWrench } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import React from "react"
 import BaseLayout from "../components/static/BaseLayout"
 import MainHeading from "../components/static/MainHeading"
@@ -8,7 +10,9 @@ const NotFoundPage: React.FC = () => {
       <MainHeading subHeading="Requested page is currently under development and will be available soon">
         Coming soon
       </MainHeading>
-      <p className="w-75">From summer 2022 to autumn 2023 the ReInHerit Digital-Hub is under pilot phase. The content of the page will be subsequently added / enhanced during this project phase. Provided resources might get unaivalable, changed or adapted. </p>
+      <p className="w-75">From summer 2022 to autumn 2023 the ReInHerit Digital-Hub is under pilot phase. The content of the hub will be subsequently added / enhanced during this project phase. Provided resources might get unaivalable, changed or adapted during that time. </p>
+      <p className="w-75">Please apologize for the inconveniences - The ReInHerit team.</p>
+      <FontAwesomeIcon icon={faWrench} size="2x"></FontAwesomeIcon>
     </BaseLayout>
   )
 }
