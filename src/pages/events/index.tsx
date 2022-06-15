8fd5bec42cc81aa74acb51220f3e0517f688b316
Sebi <SebastianStoff@gmx.at>
2022-06-15 09:34:18 +0200

    #168. added events intro text

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -5,8 +5,11 @@ import MainHeading from "../../components/static/MainHeading";
 const EventsPage: React.FC = () => {
   return <BaseLayout>
     <MainHeading
-      subHeading=""
+      subHeading="Cultural heritage events organised by ReInHerit"
     >Events overview</MainHeading>
+    <p>Here you'll find an overview of all cultural heritage related events provided via ReInHerit, like artistic performances.</p>
+
+    <p>( Coming soon )</p>
   </BaseLayout>;
 }
 
