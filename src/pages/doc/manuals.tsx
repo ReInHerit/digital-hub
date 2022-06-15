2790a936aef8faa0e7c59df2fcdf270ff0174a54
Sebi <SebastianStoff@gmx.at>
2022-06-15 13:20:21 +0200

    #176. manuals page

---
+++ src/pages/doc/manuals.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import BaseLayout from "../../components/static/BaseLayout"
+import MainHeading from "../../components/static/MainHeading"
+
+/**
+ * Renders manuals / how-to page component.
+ */
+const ManualsPage: React.FC = () => {
+  return (
+    <BaseLayout>
+      <MainHeading subHeading="Short helpflul descriptions on the functionalities of the ReInHerit digital-hub.">
+        Manuals and how-tos
+      </MainHeading>
+      <p>
+        Here you'll find small explainatory texts on how to use the diverese
+        features of this digital-hub.
+      </p>
+    </BaseLayout>
+  )
+}
+
+export default ManualsPage
