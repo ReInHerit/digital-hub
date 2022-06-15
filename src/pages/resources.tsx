6dabdb1a001ac59ac810307330888364f8ea9618
Sebi <SebastianStoff@gmx.at>
2022-06-15 08:24:53 +0200

    #168. updated reesources texts

--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -11,38 +11,38 @@ const ResourcesPage = () => {
     <BaseLayout>
 
       <MainHeading
-        subHeading="Resources available on the Digital Hub"
+        subHeading="Useful tools, educational material, guidelines, etc. for cultural heritage professionals."
       >Resources</MainHeading>
-
+      <p>Here you'll find an overview of all resources developed and provided by ReInHerit. Browse through the items and learn how to implement them in your institution.</p>
       <ReinCardGrid>
         <ReinGridCard
           title="Tool apps"
           url="/tools/apps"
-          body="coming soon..."
+          body="Applications that are either developed within the ReInHerit project or play an important role for CH management or preservation."
         />
         <ReinGridCard
           title="Tool components"
           url="/tools/components"
-          body="coming soon..."
+          body="Necessary building blocks for the applications provided by ReInHerit."
         />
         <ReinGridCard
           title="Webinars"
-          body="coming soon..."
+          body="Training material either on the tools developed in the ReInHerit-project or generally on digitization in the CH sector, CH management and preservation. "
           url="/webinars"
         />
         <ReinGridCard
           title="Game"
-          body="coming soon..."
+          body="Video game developed within the ReInHerit project."
           url="/tools"
         />
         <ReinGridCard
           title="Performances"
-          body="coming soon..."
+          body="Performances developed and carried out in the course of the ReInHerit project."
           url="/tools"
         />
         <ReinGridCard
           title="Best Practices"
-          body="coming soon..."
+          body="Guidelines, recommendations and manuals related to digital cultural heritage management."
           url="/tools"
         />
         </ReinCardGrid>
