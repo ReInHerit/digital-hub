dae6702cb7f57cb9d478814b86d279e80b343bca
Sebi <SebastianStoff@gmx.at>
2022-06-15 11:20:39 +0200

    #168. added coming soon to traveling exhibs

--- src/pages/exhibs/travel/index.tsx
+++ src/pages/exhibs/travel/index.tsx
@@ -4,6 +4,7 @@ import BaseLayout from "../../../components/static/BaseLayout"
 const TravelExhibsPage = () => {
   return <BaseLayout>
     <h1>Travelling exhibitions</h1>
+    <p>(Coming soon...)</p>
   </BaseLayout>
 
 
