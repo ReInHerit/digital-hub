c619515912325a4fedda934bc29cdf85250874c2
Sebi <SebastianStoff@gmx.at>
2022-06-15 09:30:03 +0200

    #177. added events overview page

---
+++ src/pages/events/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BaseLayout from "../../components/static/BaseLayout";
+import MainHeading from "../../components/static/MainHeading";
+
+const EventsPage: React.FC = () => {
+  return <BaseLayout>
+    <MainHeading
+      subHeading=""
+    >Events overview</MainHeading>
+  </BaseLayout>;
+}
+
+export default EventsPage;
