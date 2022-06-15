7b8a58b6ffeccea62b5c64c10500423ebcdf0e92
Sebi <SebastianStoff@gmx.at>
2022-06-15 08:40:10 +0200

    #168. added texts for tool stuff

--- src/pages/tools/apps.tsx
+++ src/pages/tools/apps.tsx
@@ -1,3 +1,4 @@
+import { Link } from "gatsby"
 import React from "react"
 import ToolAppsFeed from "../../components/shared/ToolAppsFeed"
 import BaseLayout from "../../components/static/BaseLayout"
@@ -9,7 +10,7 @@ const Toolkit = () => {
       <MainHeading
         subHeading="Applications available via the ReInHerit digital hub."
       >Tool apps</MainHeading>
-      <p>Here you'll find applications that are either developed within the ReInHerit project or play an important role for CH management or preservation.</p>
+      <p>Here you'll find applications that are either developed within the ReInHerit project or play an important role for CH management or preservation. At <Link to="/tools/components">tool components</Link> you will find the building blocks necessary for the implementation of these apps.</p>
       <ToolAppsFeed></ToolAppsFeed>
     </BaseLayout>
   )
