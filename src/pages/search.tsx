68bd82dadd1982e1aef1d1fa5fbe32e240ea374e
Sebastian Stoff <sebastianstoff@gmx.at>
2022-06-16 18:14:00 +0200

    #163. added basic state controlled search mechanism

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -3,19 +3,38 @@ import BaseLayout from "../components/static/BaseLayout"
 import MainHeading from "../components/static/MainHeading"
 import { useFlexSearch } from "react-use-flexsearch"
 import { graphql, useStaticQuery } from "gatsby"
+import { FormControl, InputGroup } from "react-bootstrap"
 
 const SearchPage: React.FC = () => {
+  const [searchQuery, setSearchQuery] = React.useState("how")
+
   const GRAPHQL_RESPONSE: SearchGraphqlResponse.Data = useStaticQuery(
     SEARCH_QUERY
   )
 
-  const result: SearchGraphqlResponse.SearchResult[] = useFlexSearch("how", GRAPHQL_RESPONSE.localSearchPages.index, GRAPHQL_RESPONSE.localSearchPages.store);
+  const result: SearchGraphqlResponse.SearchResult[] = useFlexSearch(
+    searchQuery,
+    GRAPHQL_RESPONSE.localSearchPages.index,
+    GRAPHQL_RESPONSE.localSearchPages.store
+  )
 
   return (
     <BaseLayout>
       <MainHeading>Search</MainHeading>
+      <InputGroup className="mb-3">
+        <FormControl
+          placeholder="Username"
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+          value={searchQuery}
+          onChange={(evt) => setSearchQuery(evt.currentTarget.value)}
+        />
+      </InputGroup>
+      <p>{searchQuery}</p>
       <ul>
-        {result.map(result => <li>{result.title}</li>)}
+        {result.map(result => (
+          <li>{result.title}</li>
+        ))}
       </ul>
     </BaseLayout>
   )
@@ -33,11 +52,10 @@ const SEARCH_QUERY = graphql`
 `
 
 declare module SearchGraphqlResponse {
-
   export interface SearchResult {
-      pageId: string
-      type: string
-      title: string
+    pageId: string
+    type: string
+    title: string
   }
 
   export interface Store {
