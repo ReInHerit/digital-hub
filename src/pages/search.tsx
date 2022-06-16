c3c31845744956ddcf8d3030ebbaf2cc7d6b11dd
Sebastian Stoff <sebastianstoff@gmx.at>
2022-06-16 18:00:09 +0200

    #163. implemented search page with static querying

---
+++ src/pages/search.tsx
@@ -0,0 +1,62 @@
+import React from "react"
+import BaseLayout from "../components/static/BaseLayout"
+import MainHeading from "../components/static/MainHeading"
+import { useFlexSearch } from "react-use-flexsearch"
+import { graphql, useStaticQuery } from "gatsby"
+
+const SearchPage: React.FC = () => {
+  const GRAPHQL_RESPONSE: SearchGraphqlResponse.Data = useStaticQuery(
+    SEARCH_QUERY
+  )
+
+  const result: SearchGraphqlResponse.SearchResult[] = useFlexSearch("d", GRAPHQL_RESPONSE.localSearchPages.index, GRAPHQL_RESPONSE.localSearchPages.store);
+
+  return (
+    <BaseLayout>
+      <MainHeading>Search</MainHeading>
+      <ul>
+        {result.map(result => <li>{result.title}</li>)}
+      </ul>
+    </BaseLayout>
+  )
+}
+
+export default SearchPage
+
+const SEARCH_QUERY = graphql`
+  query SEARCH_QUERY {
+    localSearchPages {
+      index
+      store
+    }
+  }
+`
+
+declare module SearchGraphqlResponse {
+
+  export interface SearchResult {
+      pageId: string
+      type: string
+      title: string
+  }
+
+  export interface Store {
+    [property: string]: SearchResult
+  }
+
+  export interface LocalSearchPages {
+    index: string
+    store: Store
+  }
+
+  export interface Data {
+    localSearchPages: LocalSearchPages
+  }
+
+  export interface Extensions {}
+
+  export interface RootObject {
+    data: Data
+    extensions: Extensions
+  }
+}
