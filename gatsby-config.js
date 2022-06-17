c82fd9ebc6c02a1d03c53613035f95cc06a5788d
Sebi <SebastianStoff@gmx.at>
2022-06-17 11:47:58 +0200

    #163. added type to index

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,7 +86,7 @@ module.exports = {
         // List of keys to index. The values of the keys are taken from the
         // normalizer function below.
         // Default: all fields
-        index: ['title','body', 'question'],
+        index: ['title','body', 'question', 'type'],
 
         // List of keys to store and make available in your UI. The values of
         // the keys are taken from the normalizer function below.
