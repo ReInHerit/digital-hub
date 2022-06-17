05a6557a219bcfb60c46be4b098a97955c43c1a4
Sebi <SebastianStoff@gmx.at>
2022-06-17 08:37:19 +0200

    #163. building search page

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import BaseLayout from "../components/static/BaseLayout"
 import MainHeading from "../components/static/MainHeading"
 import { useFlexSearch } from "react-use-flexsearch"
-import { graphql, useStaticQuery } from "gatsby"
+import { graphql, Link, useStaticQuery } from "gatsby"
 import { FormControl, InputGroup } from "react-bootstrap"
 
 const SearchPage: React.FC = () => {
@@ -18,22 +18,38 @@ const SearchPage: React.FC = () => {
     GRAPHQL_RESPONSE.localSearchPages.store
   )
 
+  /**
+   * Builds url for given collection item ased on type of item controlled by the CMS.
+   * @param collectionItem 
+   */
+  const buildCMSItemUrl = (collectionItem: SearchGraphqlResponse.SearchResult) => {
+    switch(collectionItem.type){
+      default:
+        return `/${collectionItem.type}/${collectionItem.pageId}`;
+    }
+  }
+
   return (
     <BaseLayout>
-      <MainHeading>Search</MainHeading>
+      <MainHeading
+        subHeading="Fulltext search across all content provided by the digital-hub."
+      >Search</MainHeading>
+      <p>Here you'll find a fulltext search across all collection items provided by the digital hub. Search values have to be exact matches and are based on exact occurences in the collection titles.</p>
       <InputGroup className="mb-3">
         <FormControl
-          placeholder="Username"
-          aria-label="Username"
+          placeholder="Search"
+          aria-label="Search"
           aria-describedby="basic-addon1"
           value={searchQuery}
           onChange={(evt) => setSearchQuery(evt.currentTarget.value)}
         />
       </InputGroup>
-      <p>{searchQuery}</p>
+      <p>Results found: {result.length}</p>
       <ul>
         {result.map(result => (
-          <li>{result.title}</li>
+          <li>
+            <Link to={buildCMSItemUrl(result)}>{result.title}</Link>
+          </li>
         ))}
       </ul>
     </BaseLayout>
