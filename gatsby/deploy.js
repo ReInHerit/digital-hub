5ef8670aeb3a497c397a05b83bc07e68752fbcc6
Sebi <SebastianStoff@gmx.at>
2022-06-17 13:30:04 +0200

    #188. added deployment script for gh-pages

---
+++ gatsby/deploy.js
@@ -0,0 +1,23 @@
+// Author: Sebastian Stoff
+// Desc: Small deployment script to push built gatsby files to hardcoded git-remote.
+// Used primarily to redeploy gatsby built files on github pages. 
+
+
+var ghpages = require('gh-pages');
+
+(() => {
+
+  const GIT_REPO = 'https://github.com/ReInHerit/reinherit.github.io_unpub.git';
+  const BRANCH = 'main';
+
+  console.log("*** Deploying now gatsby files to: " + GIT_REPO);
+
+  ghpages.publish('public', {
+    branch: BRANCH,
+    repo: GIT_REPO
+  }, () => {
+    console.log("*** Deployment finished");
+  });
+
+})();
+
