088b95e2495824bdd3cc6e62c8d73a3bc05ac523
Sebi <SebastianStoff@gmx.at>
2022-07-15 07:59:29 +0200

    #227. added base imprint

--- src/pages/about/imprint.tsx
+++ src/pages/about/imprint.tsx
@@ -1,4 +1,6 @@
+import { Link } from "gatsby";
 import React from "react";
+import { Container } from "react-bootstrap";
 import BaseLayout from "../../components/static/BaseLayout";
 
 
@@ -8,8 +10,24 @@ const Imprint = () => {
     <BaseLayout
       pageTitle="Imprint | ReInHerit Digital Hub"
     >
-      <h1>Imprint</h1>
-  
+      <Container>
+        <h1>Imprint</h1>
+        <h2 className="pt-2">Funding</h2>
+        <p>
+          This hub was developed in the course of the <Link to="/about">project ReInHerit</Link>, which was funded by the European Union’s Horizon 2020 research and innovation programme under grant agreement No 101004545. 
+        </p>
+        <h2 className="pt-1">Project lead and members</h2>
+        <Link to="/about/team">Team description</Link>
+        <h2 className="pt-3">Publisher, development and implementation</h2>
+        <p>Institute Centre for Information Modelling - Austrian Centre for Digital Humanities</p>
+        <p>Elisabethstraße 59/III</p>
+        <p>A-8010 Graz</p>
+        <a target="_blank" href="http://informationsmodellierung.uni-graz.at">http://informationsmodellierung.uni-graz.at</a>
+        <br/>
+        <br/>
+        <a href="mailto:digitalhub.reinherit@gmail.com">digitalhub.reinherit@gmail.com</a>
+      </Container>
+
     </BaseLayout>
   )
 
