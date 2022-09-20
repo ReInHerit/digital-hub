ac563c3665232d1b56741725ab101817a517b0da
Sebi <SebastianStoff@gmx.at>
2022-09-20 09:56:05 +0200

    #252. updated deployment script for better error-handling and display of information

--- gatsby/deploy.js
+++ gatsby/deploy.js
@@ -1,24 +1,49 @@
 // Author: Sebastian Stoff
 // Desc: Small deployment script to push built gatsby files to hardcoded git-remote.
-// Used primarily to redeploy gatsby built files on github pages. 
+// Used primarily to redeploy gatsby built files on github pages.
 
+var readline = require("readline")
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
 
-var ghpages = require('gh-pages');
+var ghpages = require("gh-pages");
 
 (() => {
+  console.log(`*****\nInitiating custom deployment script for the digital-hub...\n*****`)
+  console.log("Please make sure that you have required access rights (like write access to specific repositories), otherwise the deployment will intentionally fail. The deplyoment process / script is meant for reinherit project admins and developers. Please contact project administation if you have any problems.")
 
-  const GIT_REPO = 'https://github.com/ReInHerit/reinherit.github.io.git';
-  const BRANCH = 'main';
+  const GIT_REPO = "https://github.com/ReInHerit/reinherit.github.io.git"
+  const BRANCH = "main"
 
-  console.log("*** Deploying now gatsby files to: " + GIT_REPO);
-  console.log("*** Don't forget to build your files first! (e.g. via 'npm run build') Otherwise an outdated page-build might be deployed.")
+  rl.question(
+    "*** Do you want to automatically deploy (the built gatsby files) to the production environment? (y/n)",
+    function (answer) {
+      if (answer.toLowerCase() === "y") {
+        console.log("*** Deploying now gatsby files to: " + GIT_REPO)
+        console.log(
+          "*** Don't forget to build your files first! (e.g. via 'npm run build') Otherwise an outdated page-build might be deployed."
+        )
+        ghpages.publish(
+          "public",
+          {
+            branch: BRANCH,
+            repo: GIT_REPO,
+          },
+          () => {
+            console.log("*** Deployment finished")
+            console.log(`*****\nEnd of deployment script...\n*****`)
+          }
+        )
+      } else if (answer.toLowerCase() === "n") {
+        console.log("Deployment aborted by user...")
+        console.log(`*****\nEnd of deployment script...\n*****`)
+      } else {
+        console.log("You have to answer either with 'y' or 'n'. Aborting deployment...")
+      }
 
-  ghpages.publish('public', {
-    branch: BRANCH,
-    repo: GIT_REPO
-  }, () => {
-    console.log("*** Deployment finished");
-  });
-
-})();
-
+      rl.close();
+    }
+  )
+})()
