796128e1ad442ae5589349c24bf0bc3e4cb5c748
Chiara Zuanni <chiara.zuanni@gmail.com>
2024-07-28 21:48:30 +0200

    eshop correction

--- src/components/shared/EShopFeed/index.tsx
+++ src/components/shared/EShopFeed/index.tsx
@@ -122,7 +122,7 @@ const EshopQuery = graphql`
  
 query EshopQuery {
   allMarkdownRemark(
-    filter: { fileAbsolutePath: { regex: "/eshop/" }, frontmatter: {}}
+    filter: { fileAbsolutePath: { regex: "/(/eshop/)/" }, frontmatter: {}}
   ) {
     edges {
       node {
